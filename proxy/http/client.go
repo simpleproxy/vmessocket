82b4bbabd19d97cf6e03ab03680dce6ed68bf477
Mustafa <30045531+Machtergreifung@users.noreply.github.com>
2022-01-31 15:23:24 +0800

    Update client.go

--- proxy/http/client.go
+++ proxy/http/client.go
@@ -231,9 +231,6 @@ func setUpHTTPTunnel(ctx context.Context, dest net.Destination, target string, u
 	}
 
 	iConn := rawConn
-	if statConn, ok := iConn.(*internet.StatCouterConnection); ok {
-		iConn = statConn.Connection
-	}
 
 	nextProto := ""
 	if tlsConn, ok := iConn.(*tls.Conn); ok {
