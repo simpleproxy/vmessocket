8463344d43627bad4a28f82a54b0f9e42f01ebe9
Mustafa <30045531+Machtergreifung@users.noreply.github.com>
2022-01-31 15:22:06 +0800

    Update connection.go

--- transport/internet/connection.go
+++ transport/internet/connection.go
@@ -2,33 +2,8 @@ package internet
 
 import (
 	"net"
-
-	"github.com/vmessocket/vmessocket/features/stats"
 )
 
 type Connection interface {
 	net.Conn
-}
-
-type StatCouterConnection struct {
-	Connection
-	ReadCounter  stats.Counter
-	WriteCounter stats.Counter
-}
-
-func (c *StatCouterConnection) Read(b []byte) (int, error) {
-	nBytes, err := c.Connection.Read(b)
-	if c.ReadCounter != nil {
-		c.ReadCounter.Add(int64(nBytes))
-	}
-
-	return nBytes, err
-}
-
-func (c *StatCouterConnection) Write(b []byte) (int, error) {
-	nBytes, err := c.Connection.Write(b)
-	if c.WriteCounter != nil {
-		c.WriteCounter.Add(int64(nBytes))
-	}
-	return nBytes, err
 }
