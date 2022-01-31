83dab6c1cbcec370e63f39b521fd991dd17ecf14
Mustafa <30045531+Machtergreifung@users.noreply.github.com>
2022-02-01 00:08:24 +0800

    Update client.go

--- proxy/socks/client.go
+++ proxy/socks/client.go
@@ -7,7 +7,7 @@ import (
 	"context"
 	"time"
 
-	core "github.com/vmessocket/vmessocket"
+	"github.com/vmessocket/vmessocket/core"
 	"github.com/vmessocket/vmessocket/common"
 	"github.com/vmessocket/vmessocket/common/buf"
 	"github.com/vmessocket/vmessocket/common/net"
