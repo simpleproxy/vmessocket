c1f52c684cfb43df7f3aefd6d533a6a6db32826e
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-06 12:57:39 +0800

    Update byte.go

--- common/bitmask/byte.go
+++ common/bitmask/byte.go
@@ -2,16 +2,16 @@ package bitmask
 
 type Byte byte
 
+func (b *Byte) Clear(bb Byte) {
+	*b &= ^bb
+}
+
 func (b Byte) Has(bb Byte) bool {
 	return (b & bb) != 0
 }
 
 func (b *Byte) Set(bb Byte) {
 	*b |= bb
-}
-
-func (b *Byte) Clear(bb Byte) {
-	*b &= ^bb
 }
 
 func (b *Byte) Toggle(bb Byte) {
