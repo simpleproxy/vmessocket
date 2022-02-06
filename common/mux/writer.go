d8c5e282404ec72f248fe6db4b2a38afd39c0c0a
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-07 02:16:49 +0800

    Update writer.go

--- common/mux/writer.go
+++ common/mux/writer.go
@@ -18,6 +18,15 @@ type Writer struct {
 	transferType protocol.TransferType
 }
 
+func NewResponseWriter(id uint16, writer buf.Writer, transferType protocol.TransferType) *Writer {
+	return &Writer{
+		id:           id,
+		writer:       writer,
+		followup:     true,
+		transferType: transferType,
+	}
+}
+
 func NewWriter(id uint16, dest net.Destination, writer buf.Writer, transferType protocol.TransferType) *Writer {
 	return &Writer{
 		id:           id,
@@ -28,13 +37,36 @@ func NewWriter(id uint16, dest net.Destination, writer buf.Writer, transferType 
 	}
 }
 
-func NewResponseWriter(id uint16, writer buf.Writer, transferType protocol.TransferType) *Writer {
-	return &Writer{
-		id:           id,
-		writer:       writer,
-		followup:     true,
-		transferType: transferType,
+func writeMetaWithFrame(writer buf.Writer, meta FrameMetadata, data buf.MultiBuffer) error {
+	frame := buf.New()
+	if err := meta.WriteTo(frame); err != nil {
+		return err
 	}
+	if _, err := serial.WriteUint16(frame, uint16(data.Len())); err != nil {
+		return err
+	}
+	if len(data)+1 > 64*1024*1024 {
+		return errors.New("value too large")
+	}
+	sliceSize := len(data) + 1
+	mb2 := make(buf.MultiBuffer, 0, sliceSize)
+	mb2 = append(mb2, frame)
+	mb2 = append(mb2, data...)
+	return writer.WriteMultiBuffer(mb2)
+}
+
+func (w *Writer) Close() error {
+	meta := FrameMetadata{
+		SessionID:     w.id,
+		SessionStatus: SessionStatusEnd,
+	}
+	if w.hasError {
+		meta.Option.Set(OptionError)
+	}
+	frame := buf.New()
+	common.Must(meta.WriteTo(frame))
+	w.writer.WriteMultiBuffer(buf.MultiBuffer{frame})
+	return nil
 }
 
 func (w *Writer) getNextFrameMeta() FrameMetadata {
@@ -42,15 +74,19 @@ func (w *Writer) getNextFrameMeta() FrameMetadata {
 		SessionID: w.id,
 		Target:    w.dest,
 	}
-
 	if w.followup {
 		meta.SessionStatus = SessionStatusKeep
 	} else {
 		w.followup = true
 		meta.SessionStatus = SessionStatusNew
 	}
-
 	return meta
+}
+
+func (w *Writer) writeData(mb buf.MultiBuffer) error {
+	meta := w.getNextFrameMeta()
+	meta.Option.Set(OptionData)
+	return writeMetaWithFrame(w.writer, meta, mb)
 }
 
 func (w *Writer) writeMetaOnly() error {
@@ -62,39 +98,11 @@ func (w *Writer) writeMetaOnly() error {
 	return w.writer.WriteMultiBuffer(buf.MultiBuffer{b})
 }
 
-func writeMetaWithFrame(writer buf.Writer, meta FrameMetadata, data buf.MultiBuffer) error {
-	frame := buf.New()
-	if err := meta.WriteTo(frame); err != nil {
-		return err
-	}
-	if _, err := serial.WriteUint16(frame, uint16(data.Len())); err != nil {
-		return err
-	}
-
-	if len(data)+1 > 64*1024*1024 {
-		return errors.New("value too large")
-	}
-	sliceSize := len(data) + 1
-	mb2 := make(buf.MultiBuffer, 0, sliceSize)
-	mb2 = append(mb2, frame)
-	mb2 = append(mb2, data...)
-	return writer.WriteMultiBuffer(mb2)
-}
-
-func (w *Writer) writeData(mb buf.MultiBuffer) error {
-	meta := w.getNextFrameMeta()
-	meta.Option.Set(OptionData)
-
-	return writeMetaWithFrame(w.writer, meta, mb)
-}
-
 func (w *Writer) WriteMultiBuffer(mb buf.MultiBuffer) error {
 	defer buf.ReleaseMulti(mb)
-
 	if mb.IsEmpty() {
 		return w.writeMetaOnly()
 	}
-
 	for !mb.IsEmpty() {
 		var chunk buf.MultiBuffer
 		if w.transferType == protocol.TransferTypeStream {
@@ -108,22 +116,5 @@ func (w *Writer) WriteMultiBuffer(mb buf.MultiBuffer) error {
 			return err
 		}
 	}
-
-	return nil
-}
-
-func (w *Writer) Close() error {
-	meta := FrameMetadata{
-		SessionID:     w.id,
-		SessionStatus: SessionStatusEnd,
-	}
-	if w.hasError {
-		meta.Option.Set(OptionError)
-	}
-
-	frame := buf.New()
-	common.Must(meta.WriteTo(frame))
-
-	w.writer.WriteMultiBuffer(buf.MultiBuffer{frame})
 	return nil
 }
