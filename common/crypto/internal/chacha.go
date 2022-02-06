4dbe727ed52d8bff686cf749d88e6ec94bd37fac
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-06 16:23:09 +0800

    chore

--- common/crypto/internal/chacha.go
+++ common/crypto/internal/chacha.go
@@ -25,11 +25,9 @@ func NewChaCha20Stream(key []byte, nonce []byte, rounds int) *ChaCha20Stream {
 	s.state[1] = 0x3320646e
 	s.state[2] = 0x79622d32
 	s.state[3] = 0x6b206574
-
 	for i := 0; i < 8; i++ {
 		s.state[i+4] = binary.LittleEndian.Uint32(key[i*4 : i*4+4])
 	}
-
 	switch len(nonce) {
 	case 8:
 		s.state[14] = binary.LittleEndian.Uint32(nonce[0:])
@@ -41,7 +39,6 @@ func NewChaCha20Stream(key []byte, nonce []byte, rounds int) *ChaCha20Stream {
 	default:
 		panic("bad nonce length")
 	}
-
 	s.rounds = rounds
 	ChaCha20Block(&s.state, s.block[:], s.rounds)
 	return s
@@ -52,21 +49,17 @@ func (s *ChaCha20Stream) XORKeyStream(dst, src []byte) {
 	max := len(src)
 	for i < max {
 		gap := blockSize - s.offset
-
 		limit := i + gap
 		if limit > max {
 			limit = max
 		}
-
 		o := s.offset
 		for j := i; j < limit; j++ {
 			dst[j] = src[j] ^ s.block[o]
 			o++
 		}
-
 		i += gap
 		s.offset = o
-
 		if o == blockSize {
 			s.offset = 0
 			s.state[12]++
