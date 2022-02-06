71253f20165b5296c48ab0a52e30f9a0483c8910
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-06 13:37:51 +0800

    Update reader.go

--- common/buf/reader.go
+++ common/buf/reader.go
@@ -7,21 +7,18 @@ import (
 	"github.com/vmessocket/vmessocket/common/errors"
 )
 
-func readOneUDP(r io.Reader) (*Buffer, error) {
-	b := New()
-	for i := 0; i < 64; i++ {
-		_, err := b.ReadFrom(r)
-		if !b.IsEmpty() {
-			return b, nil
-		}
-		if err != nil {
-			b.Release()
-			return nil, err
-		}
-	}
+type BufferedReader struct {
+	Reader  Reader
+	Buffer  MultiBuffer
+	Spliter func(MultiBuffer, []byte) (MultiBuffer, int)
+}
 
-	b.Release()
-	return nil, newError("Reader returns too many empty payloads.")
+type PacketReader struct {
+	io.Reader
+}
+
+type SingleReader struct {
+	io.Reader
 }
 
 func ReadBuffer(r io.Reader) (*Buffer, error) {
@@ -34,20 +31,32 @@ func ReadBuffer(r io.Reader) (*Buffer, error) {
 	return nil, err
 }
 
-type BufferedReader struct {
-	Reader  Reader
-	Buffer  MultiBuffer
-	Spliter func(MultiBuffer, []byte) (MultiBuffer, int)
+func readOneUDP(r io.Reader) (*Buffer, error) {
+	b := New()
+	for i := 0; i < 64; i++ {
+		_, err := b.ReadFrom(r)
+		if !b.IsEmpty() {
+			return b, nil
+		}
+		if err != nil {
+			b.Release()
+			return nil, err
+		}
+	}
+	b.Release()
+	return nil, newError("Reader returns too many empty payloads.")
 }
 
 func (r *BufferedReader) BufferedBytes() int32 {
 	return r.Buffer.Len()
 }
 
-func (r *BufferedReader) ReadByte() (byte, error) {
-	var b [1]byte
-	_, err := r.Read(b[:])
-	return b[0], err
+func (r *BufferedReader) Close() error {
+	return common.Close(r.Reader)
+}
+
+func (r *BufferedReader) Interrupt() {
+	common.Interrupt(r.Reader)
 }
 
 func (r *BufferedReader) Read(b []byte) (int, error) {
@@ -55,7 +64,6 @@ func (r *BufferedReader) Read(b []byte) (int, error) {
 	if spliter == nil {
 		spliter = SplitBytes
 	}
-
 	if !r.Buffer.IsEmpty() {
 		buffer, nBytes := spliter(r.Buffer, b)
 		r.Buffer = buffer
@@ -64,27 +72,15 @@ func (r *BufferedReader) Read(b []byte) (int, error) {
 		}
 		return nBytes, nil
 	}
-
 	mb, err := r.Reader.ReadMultiBuffer()
 	if err != nil {
 		return 0, err
 	}
-
 	mb, nBytes := spliter(mb, b)
 	if !mb.IsEmpty() {
 		r.Buffer = mb
 	}
 	return nBytes, nil
-}
-
-func (r *BufferedReader) ReadMultiBuffer() (MultiBuffer, error) {
-	if !r.Buffer.IsEmpty() {
-		mb := r.Buffer
-		r.Buffer = nil
-		return mb, nil
-	}
-
-	return r.Reader.ReadMultiBuffer()
 }
 
 func (r *BufferedReader) ReadAtMost(size int32) (MultiBuffer, error) {
@@ -95,13 +91,48 @@ func (r *BufferedReader) ReadAtMost(size int32) (MultiBuffer, error) {
 		}
 		r.Buffer = mb
 	}
-
 	rb, mb := SplitSize(r.Buffer, size)
 	r.Buffer = rb
 	if r.Buffer.IsEmpty() {
 		r.Buffer = nil
 	}
 	return mb, nil
+}
+
+func (r *BufferedReader) ReadByte() (byte, error) {
+	var b [1]byte
+	_, err := r.Read(b[:])
+	return b[0], err
+}
+
+func (r *BufferedReader) ReadMultiBuffer() (MultiBuffer, error) {
+	if !r.Buffer.IsEmpty() {
+		mb := r.Buffer
+		r.Buffer = nil
+		return mb, nil
+	}
+	return r.Reader.ReadMultiBuffer()
+}
+
+func (r *PacketReader) ReadMultiBuffer() (MultiBuffer, error) {
+	b, err := readOneUDP(r.Reader)
+	if err != nil {
+		return nil, err
+	}
+	return MultiBuffer{b}, nil
+}
+
+func (r *SingleReader) ReadMultiBuffer() (MultiBuffer, error) {
+	b, err := ReadBuffer(r.Reader)
+	return MultiBuffer{b}, err
+}
+
+func (r *BufferedReader) WriteTo(writer io.Writer) (int64, error) {
+	nBytes, err := r.writeToInternal(writer)
+	if errors.Cause(err) == io.EOF {
+		return nBytes, nil
+	}
+	return nBytes, err
 }
 
 func (r *BufferedReader) writeToInternal(writer io.Writer) (int64, error) {
@@ -114,44 +145,6 @@ func (r *BufferedReader) writeToInternal(writer io.Writer) (int64, error) {
 		}
 		r.Buffer = nil
 	}
-
 	err := Copy(r.Reader, mbWriter, CountSize(&sc))
 	return sc.Size, err
-}
-
-func (r *BufferedReader) WriteTo(writer io.Writer) (int64, error) {
-	nBytes, err := r.writeToInternal(writer)
-	if errors.Cause(err) == io.EOF {
-		return nBytes, nil
-	}
-	return nBytes, err
-}
-
-func (r *BufferedReader) Interrupt() {
-	common.Interrupt(r.Reader)
-}
-
-func (r *BufferedReader) Close() error {
-	return common.Close(r.Reader)
-}
-
-type SingleReader struct {
-	io.Reader
-}
-
-func (r *SingleReader) ReadMultiBuffer() (MultiBuffer, error) {
-	b, err := ReadBuffer(r.Reader)
-	return MultiBuffer{b}, err
-}
-
-type PacketReader struct {
-	io.Reader
-}
-
-func (r *PacketReader) ReadMultiBuffer() (MultiBuffer, error) {
-	b, err := readOneUDP(r.Reader)
-	if err != nil {
-		return nil, err
-	}
-	return MultiBuffer{b}, nil
 }
