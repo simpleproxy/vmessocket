9576750b8e7a5e5e5b7789a7500d52f2f89f2606
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-07 02:13:19 +0800

    Update session.go

--- common/mux/session.go
+++ common/mux/session.go
@@ -8,6 +8,14 @@ import (
 	"github.com/vmessocket/vmessocket/common/protocol"
 )
 
+type Session struct {
+	input        buf.Reader
+	output       buf.Writer
+	parent       *SessionManager
+	ID           uint16
+	transferType protocol.TransferType
+}
+
 type SessionManager struct {
 	sync.RWMutex
 	sessions map[uint16]*Session
@@ -22,35 +30,22 @@ func NewSessionManager() *SessionManager {
 	}
 }
 
-func (m *SessionManager) Closed() bool {
-	m.RLock()
-	defer m.RUnlock()
-
-	return m.closed
-}
-
-func (m *SessionManager) Size() int {
-	m.RLock()
-	defer m.RUnlock()
-
-	return len(m.sessions)
-}
-
-func (m *SessionManager) Count() int {
-	m.RLock()
-	defer m.RUnlock()
-
-	return int(m.count)
+func (m *SessionManager) Add(s *Session) {
+	m.Lock()
+	defer m.Unlock()
+	if m.closed {
+		return
+	}
+	m.count++
+	m.sessions[s.ID] = s
 }
 
 func (m *SessionManager) Allocate() *Session {
 	m.Lock()
 	defer m.Unlock()
-
 	if m.closed {
 		return nil
 	}
-
 	m.count++
 	s := &Session{
 		ID:     m.count,
@@ -60,88 +55,6 @@ func (m *SessionManager) Allocate() *Session {
 	return s
 }
 
-func (m *SessionManager) Add(s *Session) {
-	m.Lock()
-	defer m.Unlock()
-
-	if m.closed {
-		return
-	}
-
-	m.count++
-	m.sessions[s.ID] = s
-}
-
-func (m *SessionManager) Remove(id uint16) {
-	m.Lock()
-	defer m.Unlock()
-
-	if m.closed {
-		return
-	}
-
-	delete(m.sessions, id)
-
-	if len(m.sessions) == 0 {
-		m.sessions = make(map[uint16]*Session, 16)
-	}
-}
-
-func (m *SessionManager) Get(id uint16) (*Session, bool) {
-	m.RLock()
-	defer m.RUnlock()
-
-	if m.closed {
-		return nil, false
-	}
-
-	s, found := m.sessions[id]
-	return s, found
-}
-
-func (m *SessionManager) CloseIfNoSession() bool {
-	m.Lock()
-	defer m.Unlock()
-
-	if m.closed {
-		return true
-	}
-
-	if len(m.sessions) != 0 {
-		return false
-	}
-
-	m.closed = true
-	return true
-}
-
-func (m *SessionManager) Close() error {
-	m.Lock()
-	defer m.Unlock()
-
-	if m.closed {
-		return nil
-	}
-
-	m.closed = true
-
-	for _, s := range m.sessions {
-		common.Close(s.input)
-		common.Close(s.output)
-	}
-
-	m.sessions = nil
-	return nil
-}
-
-type Session struct {
-	input        buf.Reader
-	output       buf.Writer
-	parent       *SessionManager
-	ID           uint16
-	transferType protocol.TransferType
-}
-
 func (s *Session) Close() error {
 	common.Close(s.output)
 	common.Close(s.input)
@@ -149,9 +62,77 @@ func (s *Session) Close() error {
 	return nil
 }
 
+func (m *SessionManager) Close() error {
+	m.Lock()
+	defer m.Unlock()
+	if m.closed {
+		return nil
+	}
+	m.closed = true
+	for _, s := range m.sessions {
+		common.Close(s.input)
+		common.Close(s.output)
+	}
+	m.sessions = nil
+	return nil
+}
+
+func (m *SessionManager) Closed() bool {
+	m.RLock()
+	defer m.RUnlock()
+	return m.closed
+}
+
+func (m *SessionManager) CloseIfNoSession() bool {
+	m.Lock()
+	defer m.Unlock()
+	if m.closed {
+		return true
+	}
+	if len(m.sessions) != 0 {
+		return false
+	}
+	m.closed = true
+	return true
+}
+
+func (m *SessionManager) Count() int {
+	m.RLock()
+	defer m.RUnlock()
+	return int(m.count)
+}
+
+func (m *SessionManager) Get(id uint16) (*Session, bool) {
+	m.RLock()
+	defer m.RUnlock()
+	if m.closed {
+		return nil, false
+	}
+	s, found := m.sessions[id]
+	return s, found
+}
+
 func (s *Session) NewReader(reader *buf.BufferedReader) buf.Reader {
 	if s.transferType == protocol.TransferTypeStream {
 		return NewStreamReader(reader)
 	}
 	return NewPacketReader(reader)
+}
+
+func (m *SessionManager) Remove(id uint16) {
+	m.Lock()
+	defer m.Unlock()
+	if m.closed {
+		return
+	}
+	delete(m.sessions, id)
+	if len(m.sessions) == 0 {
+		m.sessions = make(map[uint16]*Session, 16)
+	}
+}
+
+func (m *SessionManager) Size() int {
+	m.RLock()
+	defer m.RUnlock()
+	return len(m.sessions)
 }
