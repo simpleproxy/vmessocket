53a91bbab7b82495c88de4a411873548f81be16b
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-06 16:29:23 +0800

    Update aes.go

--- common/crypto/aes.go
+++ common/crypto/aes.go
@@ -7,6 +7,10 @@ import (
 	"github.com/vmessocket/vmessocket/common"
 )
 
+func NewAesCTRStream(key []byte, iv []byte) cipher.Stream {
+	return NewAesStreamMethod(key, iv, cipher.NewCTR)
+}
+
 func NewAesDecryptionStream(key []byte, iv []byte) cipher.Stream {
 	return NewAesStreamMethod(key, iv, cipher.NewCFBDecrypter)
 }
@@ -15,20 +19,16 @@ func NewAesEncryptionStream(key []byte, iv []byte) cipher.Stream {
 	return NewAesStreamMethod(key, iv, cipher.NewCFBEncrypter)
 }
 
-func NewAesStreamMethod(key []byte, iv []byte, f func(cipher.Block, []byte) cipher.Stream) cipher.Stream {
-	aesBlock, err := aes.NewCipher(key)
-	common.Must(err)
-	return f(aesBlock, iv)
-}
-
-func NewAesCTRStream(key []byte, iv []byte) cipher.Stream {
-	return NewAesStreamMethod(key, iv, cipher.NewCTR)
-}
-
 func NewAesGcm(key []byte) cipher.AEAD {
 	block, err := aes.NewCipher(key)
 	common.Must(err)
 	aead, err := cipher.NewGCM(block)
 	common.Must(err)
 	return aead
+}
+
+func NewAesStreamMethod(key []byte, iv []byte, f func(cipher.Block, []byte) cipher.Stream) cipher.Stream {
+	aesBlock, err := aes.NewCipher(key)
+	common.Must(err)
+	return f(aesBlock, iv)
 }
