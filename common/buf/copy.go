654432181d16f776b8ac6732f863c2e8e7aa42bd
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-06 13:16:23 +0800

    Update copy.go

--- common/buf/copy.go
+++ common/buf/copy.go
@@ -8,85 +8,26 @@ import (
 	"github.com/vmessocket/vmessocket/common/signal"
 )
 
-type dataHandler func(MultiBuffer)
+var ErrNotTimeoutReader = newError("not a TimeoutReader")
 
 type copyHandler struct {
 	onData []dataHandler
+}
+
+type CopyOption func(*copyHandler)
+
+type dataHandler func(MultiBuffer)
+
+type readError struct {
+	error
 }
 
 type SizeCounter struct {
 	Size int64
 }
 
-type CopyOption func(*copyHandler)
-
-func UpdateActivity(timer signal.ActivityUpdater) CopyOption {
-	return func(handler *copyHandler) {
-		handler.onData = append(handler.onData, func(MultiBuffer) {
-			timer.Update()
-		})
-	}
-}
-
-func CountSize(sc *SizeCounter) CopyOption {
-	return func(handler *copyHandler) {
-		handler.onData = append(handler.onData, func(b MultiBuffer) {
-			sc.Size += int64(b.Len())
-		})
-	}
-}
-
-type readError struct {
-	error
-}
-
-func (e readError) Error() string {
-	return e.error.Error()
-}
-
-func (e readError) Inner() error {
-	return e.error
-}
-
-func IsReadError(err error) bool {
-	_, ok := err.(readError)
-	return ok
-}
-
 type writeError struct {
 	error
-}
-
-func (e writeError) Error() string {
-	return e.error.Error()
-}
-
-func (e writeError) Inner() error {
-	return e.error
-}
-
-func IsWriteError(err error) bool {
-	_, ok := err.(writeError)
-	return ok
-}
-
-func copyInternal(reader Reader, writer Writer, handler *copyHandler) error {
-	for {
-		buffer, err := reader.ReadMultiBuffer()
-		if !buffer.IsEmpty() {
-			for _, handler := range handler.onData {
-				handler(buffer)
-			}
-
-			if werr := writer.WriteMultiBuffer(buffer); werr != nil {
-				return writeError{werr}
-			}
-		}
-
-		if err != nil {
-			return readError{err}
-		}
-	}
 }
 
 func Copy(reader Reader, writer Writer, options ...CopyOption) error {
@@ -101,7 +42,22 @@ func Copy(reader Reader, writer Writer, options ...CopyOption) error {
 	return nil
 }
 
-var ErrNotTimeoutReader = newError("not a TimeoutReader")
+func copyInternal(reader Reader, writer Writer, handler *copyHandler) error {
+	for {
+		buffer, err := reader.ReadMultiBuffer()
+		if !buffer.IsEmpty() {
+			for _, handler := range handler.onData {
+				handler(buffer)
+			}
+			if werr := writer.WriteMultiBuffer(buffer); werr != nil {
+				return writeError{werr}
+			}
+		}
+		if err != nil {
+			return readError{err}
+		}
+	}
+}
 
 func CopyOnceTimeout(reader Reader, writer Writer, timeout time.Duration) error {
 	timeoutReader, ok := reader.(TimeoutReader)
@@ -113,4 +69,46 @@ func CopyOnceTimeout(reader Reader, writer Writer, timeout time.Duration) error 
 		return err
 	}
 	return writer.WriteMultiBuffer(mb)
+}
+
+func CountSize(sc *SizeCounter) CopyOption {
+	return func(handler *copyHandler) {
+		handler.onData = append(handler.onData, func(b MultiBuffer) {
+			sc.Size += int64(b.Len())
+		})
+	}
+}
+
+func IsReadError(err error) bool {
+	_, ok := err.(readError)
+	return ok
+}
+
+func IsWriteError(err error) bool {
+	_, ok := err.(writeError)
+	return ok
+}
+
+func UpdateActivity(timer signal.ActivityUpdater) CopyOption {
+	return func(handler *copyHandler) {
+		handler.onData = append(handler.onData, func(MultiBuffer) {
+			timer.Update()
+		})
+	}
+}
+
+func (e readError) Error() string {
+	return e.error.Error()
+}
+
+func (e writeError) Error() string {
+	return e.error.Error()
+}
+
+func (e readError) Inner() error {
+	return e.error
+}
+
+func (e writeError) Inner() error {
+	return e.error
 }
