15a23fc4662e0c61e4d0559fa1844fb4233d6e2b
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-08 11:58:03 +0800

    Update time.go

--- common/protocol/time.go
+++ common/protocol/time.go
@@ -5,18 +5,18 @@ import (
 
 	"github.com/vmessocket/vmessocket/common/dice"
 )
-
-type Timestamp int64
-
-type TimestampGenerator func() Timestamp
-
-func NowTime() Timestamp {
-	return Timestamp(time.Now().Unix())
-}
+type (
+	Timestamp int64
+	TimestampGenerator func() Timestamp
+)
 
 func NewTimestampGenerator(base Timestamp, delta int) TimestampGenerator {
 	return func() Timestamp {
 		rangeInDelta := dice.Roll(delta*2) - delta
 		return base + Timestamp(rangeInDelta)
 	}
+}
+
+func NowTime() Timestamp {
+	return Timestamp(time.Now().Unix())
 }
