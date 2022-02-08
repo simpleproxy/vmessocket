434775f2856c83b9c0d364aa0d70043fe47507b8
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-08 22:38:45 +0800

    chore

--- infra/conf/cfgcommon/session.go
+++ infra/conf/cfgcommon/session.go
@@ -7,12 +7,29 @@ import (
 	"github.com/vmessocket/vmessocket/infra/conf/geodata"
 )
 
-type configureLoadingContext int
-
 const confContextKey = configureLoadingContext(1)
+
+type configureLoadingContext int
 
 type configureLoadingEnvironment struct {
 	geoLoader geodata.Loader
+}
+
+type ConfigureLoadingEnvironment interface {
+	GetGeoLoader() geodata.Loader
+}
+
+func GetConfigureLoadingEnvironment(ctx context.Context) ConfigureLoadingEnvironment {
+	return ctx.Value(confContextKey).(ConfigureLoadingEnvironment)
+}
+
+func NewConfigureLoadingContext(ctx context.Context) context.Context {
+	environment := &configureLoadingEnvironment{}
+	return context.WithValue(ctx, confContextKey, environment)
+}
+
+func SetGeoDataLoader(ctx context.Context, loader geodata.Loader) {
+	GetConfigureLoadingEnvironment(ctx).(*configureLoadingEnvironment).geoLoader = loader
 }
 
 func (c *configureLoadingEnvironment) GetGeoLoader() geodata.Loader {
@@ -22,21 +39,4 @@ func (c *configureLoadingEnvironment) GetGeoLoader() geodata.Loader {
 		common.Must(err)
 	}
 	return c.geoLoader
-}
-
-type ConfigureLoadingEnvironment interface {
-	GetGeoLoader() geodata.Loader
-}
-
-func NewConfigureLoadingContext(ctx context.Context) context.Context {
-	environment := &configureLoadingEnvironment{}
-	return context.WithValue(ctx, confContextKey, environment)
-}
-
-func GetConfigureLoadingEnvironment(ctx context.Context) ConfigureLoadingEnvironment {
-	return ctx.Value(confContextKey).(ConfigureLoadingEnvironment)
-}
-
-func SetGeoDataLoader(ctx context.Context, loader geodata.Loader) {
-	GetConfigureLoadingEnvironment(ctx).(*configureLoadingEnvironment).geoLoader = loader
 }
