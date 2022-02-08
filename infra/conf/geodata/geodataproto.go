8af7e8ac9cc51fb27c3c0226a09e9db1cb6a0d26
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-09 00:08:33 +0800

    chore

--- infra/conf/geodata/geodataproto.go
+++ infra/conf/geodata/geodataproto.go
@@ -4,14 +4,14 @@ import "github.com/vmessocket/vmessocket/app/router"
 
 //go:generate go run github.com/vmessocket/vmessocket/common/errors/errorgen
 
-type LoaderImplementation interface {
-	LoadSite(filename, list string) ([]*router.Domain, error)
-	LoadIP(filename, country string) ([]*router.CIDR, error)
-}
-
 type Loader interface {
 	LoaderImplementation
 	LoadGeoSite(list string) ([]*router.Domain, error)
 	LoadGeoSiteWithAttr(file string, siteWithAttr string) ([]*router.Domain, error)
 	LoadGeoIP(country string) ([]*router.CIDR, error)
+}
+
+type LoaderImplementation interface {
+	LoadSite(filename, list string) ([]*router.Domain, error)
+	LoadIP(filename, country string) ([]*router.CIDR, error)
 }
