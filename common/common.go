0567f6928ac61a0234035a94cd68420aaafb23b1
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-08 13:29:50 +0800

    chore

--- common/common.go
+++ common/common.go
@@ -18,21 +18,6 @@ import (
 
 var ErrNoClue = errors.New("not enough information for making a decision")
 
-func Must(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func Must2(v interface{}, err error) interface{} {
-	Must(err)
-	return v
-}
-
-func Error2(v interface{}, err error) error {
-	return err
-}
-
 func envFile() (string, error) {
 	if file := os.Getenv("GOENV"); file != "" {
 		if file == "off" {
@@ -50,29 +35,39 @@ func envFile() (string, error) {
 	return filepath.Join(dir, "go", "env"), nil
 }
 
-func GetRuntimeEnv(key string) (string, error) {
-	file, err := envFile()
+func Error2(v interface{}, err error) error {
+	return err
+}
+
+func FetchHTTPContent(target string) ([]byte, error) {
+	parsedTarget, err := url.Parse(target)
 	if err != nil {
-		return "", err
+		return nil, newError("invalid URL: ", target).Base(err)
 	}
-	if file == "" {
-		return "", fmt.Errorf("missing runtime env file")
+	if s := strings.ToLower(parsedTarget.Scheme); s != "http" && s != "https" {
+		return nil, newError("invalid scheme: ", parsedTarget.Scheme)
 	}
-	var data []byte
-	var runtimeEnv string
-	data, readErr := os.ReadFile(file)
-	if readErr != nil {
-		return "", readErr
+	client := &http.Client{
+		Timeout: 30 * time.Second,
 	}
-	envStrings := strings.Split(string(data), "\n")
-	for _, envItem := range envStrings {
-		envItem = strings.TrimSuffix(envItem, "\r")
-		envKeyValue := strings.Split(envItem, "=")
-		if strings.EqualFold(strings.TrimSpace(envKeyValue[0]), key) {
-			runtimeEnv = strings.TrimSpace(envKeyValue[1])
-		}
+	resp, err := client.Do(&http.Request{
+		Method: "GET",
+		URL:    parsedTarget,
+		Close:  true,
+	})
+	if err != nil {
+		return nil, newError("failed to dial to ", target).Base(err)
 	}
-	return runtimeEnv, nil
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, newError("unexpected HTTP status code: ", resp.StatusCode)
+	}
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, newError("failed to read HTTP response").Base(err)
+	}
+	return content, nil
 }
 
 func GetGOBIN() string {
@@ -107,37 +102,38 @@ func GetGOPATH() string {
 	return GOPATH
 }
 
-func FetchHTTPContent(target string) ([]byte, error) {
-	parsedTarget, err := url.Parse(target)
+func GetRuntimeEnv(key string) (string, error) {
+	file, err := envFile()
 	if err != nil {
-		return nil, newError("invalid URL: ", target).Base(err)
+		return "", err
 	}
+	if file == "" {
+		return "", fmt.Errorf("missing runtime env file")
+	}
+	var data []byte
+	var runtimeEnv string
+	data, readErr := os.ReadFile(file)
+	if readErr != nil {
+		return "", readErr
+	}
+	envStrings := strings.Split(string(data), "\n")
+	for _, envItem := range envStrings {
+		envItem = strings.TrimSuffix(envItem, "\r")
+		envKeyValue := strings.Split(envItem, "=")
+		if strings.EqualFold(strings.TrimSpace(envKeyValue[0]), key) {
+			runtimeEnv = strings.TrimSpace(envKeyValue[1])
+		}
+	}
+	return runtimeEnv, nil
+}
 
-	if s := strings.ToLower(parsedTarget.Scheme); s != "http" && s != "https" {
-		return nil, newError("invalid scheme: ", parsedTarget.Scheme)
-	}
-
-	client := &http.Client{
-		Timeout: 30 * time.Second,
-	}
-	resp, err := client.Do(&http.Request{
-		Method: "GET",
-		URL:    parsedTarget,
-		Close:  true,
-	})
+func Must(err error) {
 	if err != nil {
-		return nil, newError("failed to dial to ", target).Base(err)
+		panic(err)
 	}
-	defer resp.Body.Close()
+}
 
-	if resp.StatusCode != http.StatusOK {
-		return nil, newError("unexpected HTTP status code: ", resp.StatusCode)
-	}
-
-	content, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, newError("failed to read HTTP response").Base(err)
-	}
-
-	return content, nil
+func Must2(v interface{}, err error) interface{} {
+	Must(err)
+	return v
 }
