c785f1ab9f1cbfe03deeeffa7d9385a01644a7a0
Mustafa <30045531+DNS-over-HTTPS@users.noreply.github.com>
2022-02-10 00:28:18 +0800

    chore

--- infra/conf/services.go
+++ infra/conf/services.go
@@ -17,13 +17,10 @@ func (c *Config) BuildServices(service map[string]*json.RawMessage) ([]*serial.T
 		if err != nil || message == nil {
 			return nil, newError("Cannot find service", k, "").Base(err)
 		}
-
 		serviceConfig := dynamic.NewMessage(message)
-
 		if err := serviceConfig.UnmarshalJSONPB(&jsonpb.Unmarshaler{AllowUnknownFields: false}, *v); err != nil {
 			return nil, newError("Cannot interpret service configure file", k, "").Base(err)
 		}
-
 		ret = append(ret, serial.ToTypedMessage(serviceConfig))
 	}
 	return ret, nil
