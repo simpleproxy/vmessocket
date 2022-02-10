613556f96ef3b346d7aa62dae380a121a9688fdf
Mustafa <30045531+xibixibi@users.noreply.github.com>
2022-02-10 23:34:59 +0800

    Update extra.go

--- app/proxyman/inbound/extra.go
+++ app/proxyman/inbound/extra.go
@@ -14,6 +14,23 @@ func (s *Server) Close() error {
 	return nil
 }
 
+func (s *Server) Dispatch(ctx context.Context, dest net.Destination) (*transport.Link, error) {
+	if dest.Address != muxCoolAddress {
+		return s.dispatcher.Dispatch(ctx, dest)
+	}
+	opts := pipe.OptionsFromContext(ctx)
+	uplinkReader, uplinkWriter := pipe.New(opts...)
+	downlinkReader, downlinkWriter := pipe.New(opts...)
+	_, err := NewServerWorker(ctx, s.dispatcher, &transport.Link{
+		Reader: uplinkReader,
+		Writer: downlinkWriter,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &transport.Link{Reader: downlinkReader, Writer: uplinkWriter}, nil
+}
+
 func NewServer(ctx context.Context) *Server {
 	s := &Server{}
 	core.RequireFeatures(ctx, func(d routing.Dispatcher) {
