14678b8bef1f6a0057ea21c26a7053a8c622e0ba
Mustafa <30045531+xibixibi@users.noreply.github.com>
2022-02-11 00:26:02 +0800

    Update extra.go

--- app/proxyman/inbound/extra.go
+++ app/proxyman/inbound/extra.go
@@ -24,6 +24,20 @@ type ServerWorker struct {
 	sessionManager *SessionManager
 }
 
+type SessionManager struct {
+	sync.RWMutex
+	sessions map[uint16]*Session
+	count    uint16
+	closed   bool
+}
+
+func NewSessionManager() *SessionManager {
+	return &SessionManager{
+		count:    0,
+		sessions: make(map[uint16]*Session, 16),
+	}
+}
+
 func (s *Server) Close() error {
 	return nil
 }
