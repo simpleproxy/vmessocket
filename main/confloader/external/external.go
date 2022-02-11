22454b047eb2a7a347c65cb411fc38abda39a291
Mustafa <30045531+xibixibi@users.noreply.github.com>
2022-02-11 10:20:07 +0800

    chore

--- main/confloader/external/external.go
+++ main/confloader/external/external.go
@@ -21,14 +21,11 @@ func ConfigLoader(arg string) (out io.Reader, err error) {
 	switch {
 	case strings.HasPrefix(arg, "http://"), strings.HasPrefix(arg, "https://"):
 		data, err = FetchHTTPContent(arg)
-
 	case arg == "stdin:":
 		data, err = io.ReadAll(os.Stdin)
-
 	default:
 		data, err = os.ReadFile(arg)
 	}
-
 	if err != nil {
 		return
 	}
@@ -36,16 +33,22 @@ func ConfigLoader(arg string) (out io.Reader, err error) {
 	return
 }
 
+func ExtConfigLoader(files []string, reader io.Reader) (io.Reader, error) {
+	buf, err := ctlcmd.Run(append([]string{"config"}, files...), reader)
+	if err != nil {
+		return nil, err
+	}
+	return strings.NewReader(buf.String()), nil
+}
+
 func FetchHTTPContent(target string) ([]byte, error) {
 	parsedTarget, err := url.Parse(target)
 	if err != nil {
 		return nil, newError("invalid URL: ", target).Base(err)
 	}
-
 	if s := strings.ToLower(parsedTarget.Scheme); s != "http" && s != "https" {
 		return nil, newError("invalid scheme: ", parsedTarget.Scheme)
 	}
-
 	client := &http.Client{
 		Timeout: 30 * time.Second,
 	}
@@ -58,26 +61,14 @@ func FetchHTTPContent(target string) ([]byte, error) {
 		return nil, newError("failed to dial to ", target).Base(err)
 	}
 	defer resp.Body.Close()
-
 	if resp.StatusCode != 200 {
 		return nil, newError("unexpected HTTP status code: ", resp.StatusCode)
 	}
-
 	content, err := buf.ReadAllToBytes(resp.Body)
 	if err != nil {
 		return nil, newError("failed to read HTTP response").Base(err)
 	}
-
 	return content, nil
-}
-
-func ExtConfigLoader(files []string, reader io.Reader) (io.Reader, error) {
-	buf, err := ctlcmd.Run(append([]string{"config"}, files...), reader)
-	if err != nil {
-		return nil, err
-	}
-
-	return strings.NewReader(buf.String()), nil
 }
 
 func init() {
