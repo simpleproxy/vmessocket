6522ddafbeb5b08a0c51d4fb1800411f68572011
Mustafa <30045531+xibixibi@users.noreply.github.com>
2022-02-12 19:11:20 +0800

    chore

--- proxy/vmess/encoding/auth.go
+++ proxy/vmess/encoding/auth.go
@@ -11,51 +11,23 @@ import (
 	"github.com/vmessocket/vmessocket/common/crypto"
 )
 
-func Authenticate(b []byte) uint32 {
-	fnv1hash := fnv.New32a()
-	common.Must2(fnv1hash.Write(b))
-	return fnv1hash.Sum32()
-}
-
-type NoOpAuthenticator struct{}
-
-func (NoOpAuthenticator) NonceSize() int {
-	return 0
-}
-
-func (NoOpAuthenticator) Overhead() int {
-	return 0
-}
-
-func (NoOpAuthenticator) Seal(dst, nonce, plaintext, additionalData []byte) []byte {
-	return append(dst[:0], plaintext...)
-}
-
-func (NoOpAuthenticator) Open(dst, nonce, ciphertext, additionalData []byte) ([]byte, error) {
-	return append(dst[:0], ciphertext...), nil
+type AEADSizeParser struct {
+	crypto.AEADChunkSizeParser
 }
 
 type FnvAuthenticator struct{}
 
-func (*FnvAuthenticator) NonceSize() int {
-	return 0
+type NoOpAuthenticator struct{}
+
+type ShakeSizeParser struct {
+	shake  sha3.ShakeHash
+	buffer [2]byte
 }
 
-func (*FnvAuthenticator) Overhead() int {
-	return 4
-}
-
-func (*FnvAuthenticator) Seal(dst, nonce, plaintext, additionalData []byte) []byte {
-	dst = append(dst, 0, 0, 0, 0)
-	binary.BigEndian.PutUint32(dst, Authenticate(plaintext))
-	return append(dst, plaintext...)
-}
-
-func (*FnvAuthenticator) Open(dst, nonce, ciphertext, additionalData []byte) ([]byte, error) {
-	if binary.BigEndian.Uint32(ciphertext[:4]) != Authenticate(ciphertext[4:]) {
-		return dst, newError("invalid authentication")
-	}
-	return append(dst, ciphertext[4:]...), nil
+func Authenticate(b []byte) uint32 {
+	fnv1hash := fnv.New32a()
+	common.Must2(fnv1hash.Write(b))
+	return fnv1hash.Sum32()
 }
 
 func GenerateChacha20Poly1305Key(b []byte) []byte {
@@ -67,9 +39,8 @@ func GenerateChacha20Poly1305Key(b []byte) []byte {
 	return key
 }
 
-type ShakeSizeParser struct {
-	shake  sha3.ShakeHash
-	buffer [2]byte
+func NewAEADSizeParser(auth *crypto.AEADAuthenticator) *AEADSizeParser {
+	return &AEADSizeParser{crypto.AEADChunkSizeParser{Auth: auth}}
 }
 
 func NewShakeSizeParser(nonce []byte) *ShakeSizeParser {
@@ -78,15 +49,6 @@ func NewShakeSizeParser(nonce []byte) *ShakeSizeParser {
 	return &ShakeSizeParser{
 		shake: shake,
 	}
-}
-
-func (*ShakeSizeParser) SizeBytes() int32 {
-	return 2
-}
-
-func (s *ShakeSizeParser) next() uint16 {
-	common.Must2(s.shake.Read(s.buffer[:]))
-	return binary.BigEndian.Uint16(s.buffer[:])
 }
 
 func (s *ShakeSizeParser) Decode(b []byte) (uint16, error) {
@@ -101,18 +63,56 @@ func (s *ShakeSizeParser) Encode(size uint16, b []byte) []byte {
 	return b[:2]
 }
 
-func (s *ShakeSizeParser) NextPaddingLen() uint16 {
-	return s.next() % 64
-}
-
 func (s *ShakeSizeParser) MaxPaddingLen() uint16 {
 	return 64
 }
 
-type AEADSizeParser struct {
-	crypto.AEADChunkSizeParser
+func (s *ShakeSizeParser) next() uint16 {
+	common.Must2(s.shake.Read(s.buffer[:]))
+	return binary.BigEndian.Uint16(s.buffer[:])
 }
 
-func NewAEADSizeParser(auth *crypto.AEADAuthenticator) *AEADSizeParser {
-	return &AEADSizeParser{crypto.AEADChunkSizeParser{Auth: auth}}
+func (s *ShakeSizeParser) NextPaddingLen() uint16 {
+	return s.next() % 64
+}
+
+func (*FnvAuthenticator) NonceSize() int {
+	return 0
+}
+
+func (NoOpAuthenticator) NonceSize() int {
+	return 0
+}
+
+func (*FnvAuthenticator) Open(dst, nonce, ciphertext, additionalData []byte) ([]byte, error) {
+	if binary.BigEndian.Uint32(ciphertext[:4]) != Authenticate(ciphertext[4:]) {
+		return dst, newError("invalid authentication")
+	}
+	return append(dst, ciphertext[4:]...), nil
+}
+
+func (NoOpAuthenticator) Open(dst, nonce, ciphertext, additionalData []byte) ([]byte, error) {
+	return append(dst[:0], ciphertext...), nil
+}
+
+func (*FnvAuthenticator) Overhead() int {
+	return 4
+}
+
+func (NoOpAuthenticator) Overhead() int {
+	return 0
+}
+
+func (*FnvAuthenticator) Seal(dst, nonce, plaintext, additionalData []byte) []byte {
+	dst = append(dst, 0, 0, 0, 0)
+	binary.BigEndian.PutUint32(dst, Authenticate(plaintext))
+	return append(dst, plaintext...)
+}
+
+func (NoOpAuthenticator) Seal(dst, nonce, plaintext, additionalData []byte) []byte {
+	return append(dst[:0], plaintext...)
+}
+
+func (*ShakeSizeParser) SizeBytes() int32 {
+	return 2
 }
