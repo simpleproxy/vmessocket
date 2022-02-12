e982c1fb0b8e7cdcbc5a3a3f45ed2bbd12cf8353
Mustafa <30045531+xibixibi@users.noreply.github.com>
2022-02-12 12:16:48 +0800

    mux

--- app/proxyman/inbound/worker.go
+++ app/proxyman/inbound/worker.go
@@ -32,7 +32,6 @@ type dsWorker struct {
 	proxy          proxy.Inbound
 	stream         *internet.MemoryStreamConfig
 	tag            string
-	dispatcher     routing.Dispatcher
 	sniffingConfig *proxyman.SniffingConfig
 	hub            internet.Listener
 	ctx            context.Context
@@ -70,7 +69,6 @@ type udpWorker struct {
 	port           net.Port
 	tag            string
 	stream         *internet.MemoryStreamConfig
-	dispatcher     routing.Dispatcher
 	sniffingConfig *proxyman.SniffingConfig
 	checker        *task.Periodic
 	activeConn     map[connID]*udpConn
@@ -107,7 +105,7 @@ func (w *dsWorker) callback(conn internet.Connection) {
 		content.SniffingRequest.MetadataOnly = w.sniffingConfig.MetadataOnly
 	}
 	ctx = session.ContextWithContent(ctx, content)
-	if err := w.proxy.Process(ctx, net.Network_UNIX, conn, w.dispatcher); err != nil {
+	if err := w.proxy.Process(ctx, net.Network_UNIX, conn); err != nil {
 		newError("connection ends").Base(err).WriteToLog(session.ExportIDToError(ctx))
 	}
 	cancel()
@@ -151,7 +149,7 @@ func (w *tcpWorker) callback(conn internet.Connection) {
 		content.SniffingRequest.MetadataOnly = w.sniffingConfig.MetadataOnly
 	}
 	ctx = session.ContextWithContent(ctx, content)
-	if err := w.proxy.Process(ctx, net.Network_TCP, conn, w.dispatcher); err != nil {
+	if err := w.proxy.Process(ctx, net.Network_TCP, conn); err != nil {
 		newError("connection ends").Base(err).WriteToLog(session.ExportIDToError(ctx))
 	}
 	cancel()
@@ -192,7 +190,7 @@ func (w *udpWorker) callback(b *buf.Buffer, source net.Destination, originalDest
 				content.SniffingRequest.MetadataOnly = w.sniffingConfig.MetadataOnly
 			}
 			ctx = session.ContextWithContent(ctx, content)
-			if err := w.proxy.Process(ctx, net.Network_UDP, conn, w.dispatcher); err != nil {
+			if err := w.proxy.Process(ctx, net.Network_UDP, conn); err != nil {
 				newError("connection ends").Base(err).WriteToLog(session.ExportIDToError(ctx))
 			}
 			conn.Close()
