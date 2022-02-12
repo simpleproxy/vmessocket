ec3090d4a9a0c267f5dd5b6ad0fe1340123f2b34
Mustafa <30045531+xibixibi@users.noreply.github.com>
2022-02-13 01:23:44 +0800

    chore

--- proxy/vmess/inbound/inbound.go
+++ proxy/vmess/inbound/inbound.go
@@ -1,7 +1,5 @@
 package inbound
 
-//go:generate go run github.com/vmessocket/vmessocket/common/errors/errorgen
-
 import (
 	"context"
 	"io"
@@ -29,11 +27,53 @@ import (
 	"github.com/vmessocket/vmessocket/transport/internet"
 )
 
+var (
+	aeadForced     = false
+	aeadForced2022 = false
+)
+
+type Handler struct {
+	policyManager         policy.Manager
+	inboundHandlerManager feature_inbound.Manager
+	clients               *vmess.TimedUserValidator
+	usersByEmail          *userByEmail
+	detours               *DetourConfig
+	sessionHistory        *encoding.SessionHistory
+	secure                bool
+}
+
 type userByEmail struct {
 	sync.Mutex
 	cache           map[string]*protocol.MemoryUser
 	defaultLevel    uint32
 	defaultAlterIDs uint16
+}
+
+func isInsecureEncryption(s protocol.SecurityType) bool {
+	return s == protocol.SecurityType_NONE || s == protocol.SecurityType_LEGACY || s == protocol.SecurityType_UNKNOWN
+}
+
+func New(ctx context.Context, config *Config) (*Handler, error) {
+	v := core.MustFromContext(ctx)
+	handler := &Handler{
+		policyManager:         v.GetFeature(policy.ManagerType()).(policy.Manager),
+		inboundHandlerManager: v.GetFeature(feature_inbound.ManagerType()).(feature_inbound.Manager),
+		clients:               vmess.NewTimedUserValidator(protocol.DefaultIDHash),
+		detours:               config.Detour,
+		usersByEmail:          newUserByEmail(config.GetDefaultValue()),
+		sessionHistory:        encoding.NewSessionHistory(),
+		secure:                config.SecureEncryptionOnly,
+	}
+	for _, user := range config.User {
+		mUser, err := user.ToMemoryUser()
+		if err != nil {
+			return nil, newError("failed to get VMess user").Base(err)
+		}
+		if err := handler.AddUser(ctx, mUser); err != nil {
+			return nil, newError("failed to initiate user").Base(err)
+		}
+	}
+	return handler, nil
 }
 
 func newUserByEmail(config *DefaultConfig) *userByEmail {
@@ -42,6 +82,39 @@ func newUserByEmail(config *DefaultConfig) *userByEmail {
 		defaultLevel:    config.Level,
 		defaultAlterIDs: uint16(config.AlterId),
 	}
+}
+
+func transferResponse(timer signal.ActivityUpdater, session *encoding.ServerSession, request *protocol.RequestHeader, response *protocol.ResponseHeader, input buf.Reader, output *buf.BufferedWriter) error {
+	session.EncodeResponseHeader(response, output)
+	bodyWriter := session.EncodeResponseBody(request, output)
+	{
+		data, err := input.ReadMultiBuffer()
+		if err != nil {
+			return err
+		}
+		if err := bodyWriter.WriteMultiBuffer(data); err != nil {
+			return err
+		}
+	}
+	if err := output.SetBuffered(false); err != nil {
+		return err
+	}
+	if err := buf.Copy(input, bodyWriter, buf.UpdateActivity(timer)); err != nil {
+		return err
+	}
+	account := request.User.Account.(*vmess.MemoryAccount)
+	if request.Option.Has(protocol.RequestOptionChunkStream) && !account.NoTerminationSignal {
+		if err := bodyWriter.WriteMultiBuffer(buf.MultiBuffer{}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (v *userByEmail) Add(u *protocol.MemoryUser) bool {
+	v.Lock()
+	defer v.Unlock()
+	return v.addNoLock(u)
 }
 
 func (v *userByEmail) addNoLock(u *protocol.MemoryUser) bool {
@@ -54,19 +127,58 @@ func (v *userByEmail) addNoLock(u *protocol.MemoryUser) bool {
 	return true
 }
 
-func (v *userByEmail) Add(u *protocol.MemoryUser) bool {
-	v.Lock()
-	defer v.Unlock()
+func (h *Handler) AddUser(ctx context.Context, user *protocol.MemoryUser) error {
+	if len(user.Email) > 0 && !h.usersByEmail.Add(user) {
+		return newError("User ", user.Email, " already exists.")
+	}
+	return h.clients.Add(user)
+}
 
-	return v.addNoLock(u)
+func (h *Handler) Close() error {
+	return errors.Combine(
+		h.clients.Close(),
+		h.sessionHistory.Close(),
+		common.Close(h.usersByEmail))
+}
+
+func (h *Handler) generateCommand(ctx context.Context, request *protocol.RequestHeader) protocol.ResponseCommand {
+	if h.detours != nil {
+		tag := h.detours.To
+		if h.inboundHandlerManager != nil {
+			handler, err := h.inboundHandlerManager.GetHandler(ctx, tag)
+			if err != nil {
+				newError("failed to get detour handler: ", tag).Base(err).AtWarning().WriteToLog(session.ExportIDToError(ctx))
+				return nil
+			}
+			proxyHandler, port, availableMin := handler.GetRandomInboundProxy()
+			inboundHandler, ok := proxyHandler.(*Handler)
+			if ok && inboundHandler != nil {
+				if availableMin > 255 {
+					availableMin = 255
+				}
+				newError("pick detour handler for port ", port, " for ", availableMin, " minutes.").AtDebug().WriteToLog(session.ExportIDToError(ctx))
+				user := inboundHandler.GetUser(request.User.Email)
+				if user == nil {
+					return nil
+				}
+				account := user.Account.(*vmess.MemoryAccount)
+				return &protocol.CommandSwitchAccount{
+					Port:     port,
+					ID:       account.ID.UUID(),
+					AlterIds: uint16(len(account.AlterIDs)),
+					Level:    user.Level,
+					ValidMin: byte(availableMin),
+				}
+			}
+		}
+	}
+	return nil
 }
 
 func (v *userByEmail) Get(email string) (*protocol.MemoryUser, bool) {
 	email = strings.ToLower(email)
-
 	v.Lock()
 	defer v.Unlock()
-
 	user, found := v.cache[email]
 	if !found {
 		id := uuid.New()
@@ -86,66 +198,6 @@ func (v *userByEmail) Get(email string) (*protocol.MemoryUser, bool) {
 	return user, found
 }
 
-func (v *userByEmail) Remove(email string) bool {
-	email = strings.ToLower(email)
-
-	v.Lock()
-	defer v.Unlock()
-
-	if _, found := v.cache[email]; !found {
-		return false
-	}
-	delete(v.cache, email)
-	return true
-}
-
-type Handler struct {
-	policyManager         policy.Manager
-	inboundHandlerManager feature_inbound.Manager
-	clients               *vmess.TimedUserValidator
-	usersByEmail          *userByEmail
-	detours               *DetourConfig
-	sessionHistory        *encoding.SessionHistory
-	secure                bool
-}
-
-func New(ctx context.Context, config *Config) (*Handler, error) {
-	v := core.MustFromContext(ctx)
-	handler := &Handler{
-		policyManager:         v.GetFeature(policy.ManagerType()).(policy.Manager),
-		inboundHandlerManager: v.GetFeature(feature_inbound.ManagerType()).(feature_inbound.Manager),
-		clients:               vmess.NewTimedUserValidator(protocol.DefaultIDHash),
-		detours:               config.Detour,
-		usersByEmail:          newUserByEmail(config.GetDefaultValue()),
-		sessionHistory:        encoding.NewSessionHistory(),
-		secure:                config.SecureEncryptionOnly,
-	}
-
-	for _, user := range config.User {
-		mUser, err := user.ToMemoryUser()
-		if err != nil {
-			return nil, newError("failed to get VMess user").Base(err)
-		}
-
-		if err := handler.AddUser(ctx, mUser); err != nil {
-			return nil, newError("failed to initiate user").Base(err)
-		}
-	}
-
-	return handler, nil
-}
-
-func (h *Handler) Close() error {
-	return errors.Combine(
-		h.clients.Close(),
-		h.sessionHistory.Close(),
-		common.Close(h.usersByEmail))
-}
-
-func (*Handler) Network() []net.Network {
-	return []net.Network{net.Network_TCP, net.Network_UNIX}
-}
-
 func (h *Handler) GetUser(email string) *protocol.MemoryUser {
 	user, existing := h.usersByEmail.Get(email)
 	if !existing {
@@ -154,61 +206,8 @@ func (h *Handler) GetUser(email string) *protocol.MemoryUser {
 	return user
 }
 
-func (h *Handler) AddUser(ctx context.Context, user *protocol.MemoryUser) error {
-	if len(user.Email) > 0 && !h.usersByEmail.Add(user) {
-		return newError("User ", user.Email, " already exists.")
-	}
-	return h.clients.Add(user)
-}
-
-func (h *Handler) RemoveUser(ctx context.Context, email string) error {
-	if email == "" {
-		return newError("Email must not be empty.")
-	}
-	if !h.usersByEmail.Remove(email) {
-		return newError("User ", email, " not found.")
-	}
-	h.clients.Remove(email)
-	return nil
-}
-
-func transferResponse(timer signal.ActivityUpdater, session *encoding.ServerSession, request *protocol.RequestHeader, response *protocol.ResponseHeader, input buf.Reader, output *buf.BufferedWriter) error {
-	session.EncodeResponseHeader(response, output)
-
-	bodyWriter := session.EncodeResponseBody(request, output)
-
-	{
-		data, err := input.ReadMultiBuffer()
-		if err != nil {
-			return err
-		}
-
-		if err := bodyWriter.WriteMultiBuffer(data); err != nil {
-			return err
-		}
-	}
-
-	if err := output.SetBuffered(false); err != nil {
-		return err
-	}
-
-	if err := buf.Copy(input, bodyWriter, buf.UpdateActivity(timer)); err != nil {
-		return err
-	}
-
-	account := request.User.Account.(*vmess.MemoryAccount)
-
-	if request.Option.Has(protocol.RequestOptionChunkStream) && !account.NoTerminationSignal {
-		if err := bodyWriter.WriteMultiBuffer(buf.MultiBuffer{}); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func isInsecureEncryption(s protocol.SecurityType) bool {
-	return s == protocol.SecurityType_NONE || s == protocol.SecurityType_LEGACY || s == protocol.SecurityType_UNKNOWN
+func (*Handler) Network() []net.Network {
+	return []net.Network{net.Network_TCP, net.Network_UNIX}
 }
 
 func (h *Handler) Process(ctx context.Context, network net.Network, connection internet.Connection, dispatcher routing.Dispatcher) error {
@@ -216,7 +215,6 @@ func (h *Handler) Process(ctx context.Context, network net.Network, connection i
 	if err := connection.SetReadDeadline(time.Now().Add(sessionPolicy.Timeouts.Handshake)); err != nil {
 		return newError("unable to set read deadline").Base(err).AtWarning()
 	}
-
 	reader := &buf.BufferedReader{Reader: buf.NewReader(connection)}
 	svrSession := encoding.NewServerSession(h.clients, h.sessionHistory)
 	svrSession.SetAEADForced(aeadForced)
@@ -233,7 +231,6 @@ func (h *Handler) Process(ctx context.Context, network net.Network, connection i
 		}
 		return err
 	}
-
 	if h.secure && isInsecureEncryption(request.Security) {
 		log.Record(&log.AccessMessage{
 			From:   connection.RemoteAddr(),
@@ -244,7 +241,6 @@ func (h *Handler) Process(ctx context.Context, network net.Network, connection i
 		})
 		return newError("client is using insecure encryption: ", request.Security)
 	}
-	
 	ctx = log.ContextWithAccessMessage(ctx, &log.AccessMessage{
 		From:   connection.RemoteAddr(),
 		To:     request.Destination(),
@@ -252,115 +248,80 @@ func (h *Handler) Process(ctx context.Context, network net.Network, connection i
 		Reason: "",
 		Email:  request.User.Email,
 	})
-
 	newError("received request for ", request.Destination()).WriteToLog(session.ExportIDToError(ctx))
-
 	if err := connection.SetReadDeadline(time.Time{}); err != nil {
 		newError("unable to set back read deadline").Base(err).WriteToLog(session.ExportIDToError(ctx))
 	}
-
 	inbound := session.InboundFromContext(ctx)
 	if inbound == nil {
 		panic("no inbound metadata")
 	}
 	inbound.User = request.User
-
 	sessionPolicy = h.policyManager.ForLevel(request.User.Level)
-
 	ctx, cancel := context.WithCancel(ctx)
 	timer := signal.CancelAfterInactivity(ctx, cancel, sessionPolicy.Timeouts.ConnectionIdle)
-
 	ctx = policy.ContextWithBufferPolicy(ctx, sessionPolicy.Buffer)
 	link, err := dispatcher.Dispatch(ctx, request.Destination())
 	if err != nil {
 		return newError("failed to dispatch request to ", request.Destination()).Base(err)
 	}
-
 	requestDone := func() error {
 		defer timer.SetTimeout(sessionPolicy.Timeouts.DownlinkOnly)
-
 		bodyReader := svrSession.DecodeRequestBody(request, reader)
 		if err := buf.Copy(bodyReader, link.Writer, buf.UpdateActivity(timer)); err != nil {
 			return newError("failed to transfer request").Base(err)
 		}
 		return nil
 	}
-
 	responseDone := func() error {
 		defer timer.SetTimeout(sessionPolicy.Timeouts.UplinkOnly)
-
 		writer := buf.NewBufferedWriter(buf.NewWriter(connection))
 		defer writer.Flush()
-
 		response := &protocol.ResponseHeader{
 			Command: h.generateCommand(ctx, request),
 		}
 		return transferResponse(timer, svrSession, request, response, link.Reader, writer)
 	}
-
 	requestDonePost := task.OnSuccess(requestDone, task.Close(link.Writer))
 	if err := task.Run(ctx, requestDonePost, responseDone); err != nil {
 		common.Interrupt(link.Reader)
 		common.Interrupt(link.Writer)
 		return newError("connection ends").Base(err)
 	}
-
 	return nil
 }
 
-func (h *Handler) generateCommand(ctx context.Context, request *protocol.RequestHeader) protocol.ResponseCommand {
-	if h.detours != nil {
-		tag := h.detours.To
-		if h.inboundHandlerManager != nil {
-			handler, err := h.inboundHandlerManager.GetHandler(ctx, tag)
-			if err != nil {
-				newError("failed to get detour handler: ", tag).Base(err).AtWarning().WriteToLog(session.ExportIDToError(ctx))
-				return nil
-			}
-			proxyHandler, port, availableMin := handler.GetRandomInboundProxy()
-			inboundHandler, ok := proxyHandler.(*Handler)
-			if ok && inboundHandler != nil {
-				if availableMin > 255 {
-					availableMin = 255
-				}
-
-				newError("pick detour handler for port ", port, " for ", availableMin, " minutes.").AtDebug().WriteToLog(session.ExportIDToError(ctx))
-				user := inboundHandler.GetUser(request.User.Email)
-				if user == nil {
-					return nil
-				}
-				account := user.Account.(*vmess.MemoryAccount)
-				return &protocol.CommandSwitchAccount{
-					Port:     port,
-					ID:       account.ID.UUID(),
-					AlterIds: uint16(len(account.AlterIDs)),
-					Level:    user.Level,
-					ValidMin: byte(availableMin),
-				}
-			}
-		}
+func (v *userByEmail) Remove(email string) bool {
+	email = strings.ToLower(email)
+	v.Lock()
+	defer v.Unlock()
+	if _, found := v.cache[email]; !found {
+		return false
 	}
-
-	return nil
+	delete(v.cache, email)
+	return true
 }
 
-var (
-	aeadForced     = false
-	aeadForced2022 = false
-)
+func (h *Handler) RemoveUser(ctx context.Context, email string) error {
+	if email == "" {
+		return newError("Email must not be empty.")
+	}
+	if !h.usersByEmail.Remove(email) {
+		return newError("User ", email, " not found.")
+	}
+	h.clients.Remove(email)
+	return nil
+}
 
 func init() {
 	common.Must(common.RegisterConfig((*Config)(nil), func(ctx context.Context, config interface{}) (interface{}, error) {
 		return New(ctx, config.(*Config))
 	}))
-	
 	defaultFlagValue := "true_by_default_2022"
-
 	isAeadForced := platform.NewEnvFlag("vmessocket.vmess.aead.forced").GetValue(func() string { return defaultFlagValue })
 	if isAeadForced == "true" {
 		aeadForced = true
 	}
-
 	if isAeadForced == "true_by_default_2022" {
 		aeadForced = true
 		aeadForced2022 = true
