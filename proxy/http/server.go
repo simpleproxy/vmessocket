bbc8f39eb25e7528f12aac4ecfea7bab4d0d4fb7
Mustafa <30045531+xibixibi@users.noreply.github.com>
2022-02-13 02:46:39 +0800

    mux

--- proxy/http/server.go
+++ proxy/http/server.go
@@ -108,7 +108,7 @@ func (s *Server) handleConnect(ctx context.Context, _ *http.Request, reader *buf
 	return nil
 }
 
-func (s *Server) handlePlainHTTP(ctx context.Context, request *http.Request, writer io.Writer, dest net.Destination, dispatcher routing.Dispatcher) error {
+func (s *Server) handlePlainHTTP(ctx context.Context, request *http.Request, writer io.Writer, dest net.Destination) error {
 	var link *transport.Link
         if !s.config.AllowTransparent && request.URL.Host == "" {
 		response := &http.Response{
