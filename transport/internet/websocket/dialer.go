42891f6653256eefc9c0b4d73991dc01b19faa33
Mustafa <30045531+xibixibi@users.noreply.github.com>
2022-02-13 21:05:27 +0800

    chore

--- transport/internet/websocket/dialer.go
+++ transport/internet/websocket/dialer.go
@@ -17,9 +17,19 @@ import (
 	"github.com/vmessocket/vmessocket/transport/internet/tls"
 )
 
+type dialerWithEarlyData struct {
+	dialer  *websocket.Dialer
+	uriBase string
+	config  *Config
+}
+
+type dialerWithEarlyDataRelayed struct {
+	uriBase string
+	config  *Config
+}
+
 func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (internet.Connection, error) {
 	newError("creating connection to ", dest).WriteToLog(session.ExportIDToError(ctx))
-
 	conn, err := dialWebsocket(ctx, dest, streamSettings)
 	if err != nil {
 		return nil, newError("failed to dial WebSocket").Base(err)
@@ -27,13 +37,8 @@ func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.Me
 	return internet.Connection(conn), nil
 }
 
-func init() {
-	common.Must(internet.RegisterTransportDialer(protocolName, Dial))
-}
-
 func dialWebsocket(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (net.Conn, error) {
 	wsSettings := streamSettings.ProtocolSettings.(*Config)
-
 	dialer := &websocket.Dialer{
 		NetDial: func(network, addr string) (net.Conn, error) {
 			return internet.DialSystem(ctx, dest, streamSettings.SocketSettings)
@@ -42,20 +47,16 @@ func dialWebsocket(ctx context.Context, dest net.Destination, streamSettings *in
 		WriteBufferSize:  4 * 1024,
 		HandshakeTimeout: time.Second * 8,
 	}
-
 	protocol := "ws"
-
 	if config := tls.ConfigFromStreamSettings(streamSettings); config != nil {
 		protocol = "wss"
 		dialer.TLSClientConfig = config.GetTLSConfig(tls.WithDestination(dest), tls.WithNextProto("http/1.1"))
 	}
-
 	host := dest.NetAddr()
 	if (protocol == "ws" && dest.Port == 80) || (protocol == "wss" && dest.Port == 443) {
 		host = dest.Address.String()
 	}
 	uri := protocol + "://" + host + wsSettings.GetNormalizedPath()
-
 	if wsSettings.MaxEarlyData != 0 {
 		return newConnectionWithDelayedDial(&dialerWithEarlyData{
 			dialer:  dialer,
@@ -63,7 +64,6 @@ func dialWebsocket(ctx context.Context, dest net.Destination, streamSettings *in
 			config:  wsSettings,
 		}), nil
 	}
-
 	conn, resp, err := dialer.Dial(uri, wsSettings.GetRequestHeader())
 	if err != nil {
 		var reason string
@@ -72,35 +72,24 @@ func dialWebsocket(ctx context.Context, dest net.Destination, streamSettings *in
 		}
 		return nil, newError("failed to dial to (", uri, "): ", reason).Base(err)
 	}
-
 	return newConnection(conn, conn.RemoteAddr()), nil
-}
-
-type dialerWithEarlyData struct {
-	dialer  *websocket.Dialer
-	uriBase string
-	config  *Config
 }
 
 func (d dialerWithEarlyData) Dial(earlyData []byte) (*websocket.Conn, error) {
 	earlyDataBuf := bytes.NewBuffer(nil)
 	base64EarlyDataEncoder := base64.NewEncoder(base64.RawURLEncoding, earlyDataBuf)
-
 	earlydata := bytes.NewReader(earlyData)
 	limitedEarlyDatareader := io.LimitReader(earlydata, int64(d.config.MaxEarlyData))
 	n, encerr := io.Copy(base64EarlyDataEncoder, limitedEarlyDatareader)
 	if encerr != nil {
 		return nil, newError("websocket delayed dialer cannot encode early data").Base(encerr)
 	}
-
 	if errc := base64EarlyDataEncoder.Close(); errc != nil {
 		return nil, newError("websocket delayed dialer cannot encode early data tail").Base(errc)
 	}
-
 	dialFunction := func() (*websocket.Conn, *http.Response, error) {
 		return d.dialer.Dial(d.uriBase+earlyDataBuf.String(), d.config.GetRequestHeader())
 	}
-
 	if d.config.EarlyDataHeaderName != "" {
 		dialFunction = func() (*websocket.Conn, *http.Response, error) {
 			earlyDataStr := earlyDataBuf.String()
@@ -109,7 +98,6 @@ func (d dialerWithEarlyData) Dial(earlyData []byte) (*websocket.Conn, error) {
 			return d.dialer.Dial(d.uriBase, currentHeader)
 		}
 	}
-
 	conn, resp, err := dialFunction()
 	if err != nil {
 		var reason string
@@ -126,30 +114,26 @@ func (d dialerWithEarlyData) Dial(earlyData []byte) (*websocket.Conn, error) {
 	return conn, nil
 }
 
-type dialerWithEarlyDataRelayed struct {
-	uriBase string
-	config  *Config
-}
-
 func (d dialerWithEarlyDataRelayed) Dial(earlyData []byte) (io.ReadWriteCloser, error) {
 	earlyDataBuf := bytes.NewBuffer(nil)
 	base64EarlyDataEncoder := base64.NewEncoder(base64.RawURLEncoding, earlyDataBuf)
-
 	earlydata := bytes.NewReader(earlyData)
 	limitedEarlyDatareader := io.LimitReader(earlydata, int64(d.config.MaxEarlyData))
 	_, encerr := io.Copy(base64EarlyDataEncoder, limitedEarlyDatareader)
 	if encerr != nil {
 		return nil, newError("websocket delayed dialer cannot encode early data").Base(encerr)
 	}
-
 	if errc := base64EarlyDataEncoder.Close(); errc != nil {
 		return nil, newError("websocket delayed dialer cannot encode early data tail").Base(errc)
 	}
-
 	if d.config.EarlyDataHeaderName != "" {
 		earlyDataStr := earlyDataBuf.String()
 		currentHeader := d.config.GetRequestHeader()
 		currentHeader.Set(d.config.EarlyDataHeaderName, earlyDataStr)
 	}
 	return nil, nil
+}
+
+func init() {
+	common.Must(internet.RegisterTransportDialer(protocolName, Dial))
 }
