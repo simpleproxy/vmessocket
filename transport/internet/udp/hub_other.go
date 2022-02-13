14c231c2a6fdac92170c7b7fb37fb23745597e95
Mustafa <30045531+xibixibi@users.noreply.github.com>
2022-02-13 22:09:21 +0800

    chore

--- transport/internet/udp/hub_other.go
+++ transport/internet/udp/hub_other.go
@@ -3,15 +3,13 @@
 
 package udp
 
-import (
-	"github.com/vmessocket/vmessocket/common/net"
-)
-
-func RetrieveOriginalDest(oob []byte) net.Destination {
-	return net.Destination{}
-}
+import "github.com/vmessocket/vmessocket/common/net"
 
 func ReadUDPMsg(conn *net.UDPConn, payload []byte, oob []byte) (int, int, int, *net.UDPAddr, error) {
 	nBytes, addr, err := conn.ReadFromUDP(payload)
 	return nBytes, 0, 0, addr, err
+}
+
+func RetrieveOriginalDest(oob []byte) net.Destination {
+	return net.Destination{}
 }
