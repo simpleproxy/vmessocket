c2db2ef35d6d898bf2f4a235357760b5bcd76565
Mustafa <30045531+xibixibi@users.noreply.github.com>
2022-02-13 20:34:46 +0800

    chore

--- transport/internet/tcp_hub.go
+++ transport/internet/tcp_hub.go
@@ -8,43 +8,14 @@ import (
 
 var transportListenerCache = make(map[string]ListenFunc)
 
-func RegisterTransportListener(protocol string, listener ListenFunc) error {
-	if _, found := transportListenerCache[protocol]; found {
-		return newError(protocol, " listener already registered.").AtError()
-	}
-	transportListenerCache[protocol] = listener
-	return nil
-}
-
 type ConnHandler func(Connection)
-
-type ListenFunc func(ctx context.Context, address net.Address, port net.Port, settings *MemoryStreamConfig, handler ConnHandler) (Listener, error)
 
 type Listener interface {
 	Close() error
 	Addr() net.Addr
 }
 
-func ListenUnix(ctx context.Context, address net.Address, settings *MemoryStreamConfig, handler ConnHandler) (Listener, error) {
-	if settings == nil {
-		s, err := ToMemoryStreamConfig(nil)
-		if err != nil {
-			return nil, newError("failed to create default unix stream settings").Base(err)
-		}
-		settings = s
-	}
-
-	protocol := settings.ProtocolName
-	listenFunc := transportListenerCache[protocol]
-	if listenFunc == nil {
-		return nil, newError(protocol, " unix istener not registered.").AtError()
-	}
-	listener, err := listenFunc(ctx, address, net.Port(0), settings, handler)
-	if err != nil {
-		return nil, newError("failed to listen on unix address: ", address).Base(err)
-	}
-	return listener, nil
-}
+type ListenFunc func(ctx context.Context, address net.Address, port net.Port, settings *MemoryStreamConfig, handler ConnHandler) (Listener, error)
 
 func ListenTCP(ctx context.Context, address net.Address, port net.Port, settings *MemoryStreamConfig, handler ConnHandler) (Listener, error) {
 	if settings == nil {
@@ -54,15 +25,12 @@ func ListenTCP(ctx context.Context, address net.Address, port net.Port, settings
 		}
 		settings = s
 	}
-
 	if address.Family().IsDomain() && address.Domain() == "localhost" {
 		address = net.LocalHostIP
 	}
-
 	if address.Family().IsDomain() {
 		return nil, newError("domain address is not allowed for listening: ", address.Domain())
 	}
-
 	protocol := settings.ProtocolName
 	listenFunc := transportListenerCache[protocol]
 	if listenFunc == nil {
@@ -81,4 +49,32 @@ func ListenSystem(ctx context.Context, addr net.Addr, sockopt *SocketConfig) (ne
 
 func ListenSystemPacket(ctx context.Context, addr net.Addr, sockopt *SocketConfig) (net.PacketConn, error) {
 	return effectiveListener.ListenPacket(ctx, addr, sockopt)
+}
+
+func ListenUnix(ctx context.Context, address net.Address, settings *MemoryStreamConfig, handler ConnHandler) (Listener, error) {
+	if settings == nil {
+		s, err := ToMemoryStreamConfig(nil)
+		if err != nil {
+			return nil, newError("failed to create default unix stream settings").Base(err)
+		}
+		settings = s
+	}
+	protocol := settings.ProtocolName
+	listenFunc := transportListenerCache[protocol]
+	if listenFunc == nil {
+		return nil, newError(protocol, " unix istener not registered.").AtError()
+	}
+	listener, err := listenFunc(ctx, address, net.Port(0), settings, handler)
+	if err != nil {
+		return nil, newError("failed to listen on unix address: ", address).Base(err)
+	}
+	return listener, nil
+}
+
+func RegisterTransportListener(protocol string, listener ListenFunc) error {
+	if _, found := transportListenerCache[protocol]; found {
+		return newError(protocol, " listener already registered.").AtError()
+	}
+	transportListenerCache[protocol] = listener
+	return nil
 }
