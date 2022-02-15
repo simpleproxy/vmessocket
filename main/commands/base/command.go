c848d3e25145e256a2a525ed2690c94de10a49b4
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-02-15 17:00:10 +0800

    ch

--- main/commands/base/command.go
+++ main/commands/base/command.go
@@ -14,43 +14,16 @@ import (
 	"sync"
 )
 
-// A Command is an implementation of a v2ray command
-// like v2ray run or v2ray version.
 type Command struct {
-	// Run runs the command.
-	// The args are the arguments after the command name.
 	Run func(cmd *Command, args []string)
-
-	// UsageLine is the one-line usage message.
-	// The words between the first word (the "executable name") and the first flag or argument in the line are taken to be the command name.
-	//
-	// UsageLine supports go template syntax. It's recommended to use "{{.Exec}}" instead of hardcoding name
 	UsageLine string
-
-	// Short is the short description shown in the 'go help' output.
-	//
-	// Note: Short does not support go template syntax.
 	Short string
-
-	// Long is the long message shown in the 'go help <this-command>' output.
-	//
-	// Long supports go template syntax. It's recommended to use "{{.Exec}}", "{{.LongName}}" instead of hardcoding strings
 	Long string
-
-	// Flag is a set of flags specific to this command.
 	Flag flag.FlagSet
-
-	// CustomFlags indicates that the command will do its own
-	// flag parsing.
 	CustomFlags bool
-
-	// Commands lists the available commands and help topics.
-	// The order here is the order in which they are printed by 'go help'.
-	// Note that subcommands are in general best avoided.
 	Commands []*Command
 }
 
-// LongName returns the command's long name: all the words in the usage line between first word (e.g. "v2ray") and a flag or argument,
 func (c *Command) LongName() string {
 	name := c.UsageLine
 	if i := strings.Index(name, " ["); i >= 0 {
@@ -64,7 +37,6 @@ func (c *Command) LongName() string {
 	return strings.TrimSpace(name)
 }
 
-// Name returns the command's short name: the last word in the usage line before a flag or argument.
 func (c *Command) Name() string {
 	name := c.LongName()
 	if i := strings.LastIndex(name, " "); i >= 0 {
@@ -73,7 +45,6 @@ func (c *Command) Name() string {
 	return strings.TrimSpace(name)
 }
 
-// Usage prints usage of the Command
 func (c *Command) Usage() {
 	buildCommandText(c)
 	fmt.Fprintf(os.Stderr, "usage: %s\n", c.UsageLine)
@@ -82,31 +53,25 @@ func (c *Command) Usage() {
 	Exit()
 }
 
-// Runnable reports whether the command can be run; otherwise
-// it is a documentation pseudo-command such as importpath.
 func (c *Command) Runnable() bool {
 	return c.Run != nil
 }
 
-// Exit exits with code set with SetExitStatus()
 func Exit() {
 	os.Exit(exitStatus)
 }
 
-// Fatalf logs error and exit with code 1
 func Fatalf(format string, args ...interface{}) {
 	Errorf(format, args...)
 	Exit()
 }
 
-// Errorf logs error and set exit status to 1, but not exit
 func Errorf(format string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, format, args...)
 	fmt.Fprintln(os.Stderr)
 	SetExitStatus(1)
 }
 
-// ExitIfErrors exits if current status is not zero
 func ExitIfErrors() {
 	if exitStatus != 0 {
 		Exit()
@@ -118,7 +83,6 @@ var (
 	exitMu     sync.Mutex
 )
 
-// SetExitStatus set exit status code
 func SetExitStatus(n int) {
 	exitMu.Lock()
 	if exitStatus < n {
@@ -127,7 +91,6 @@ func SetExitStatus(n int) {
 	exitMu.Unlock()
 }
 
-// GetExitStatus get exit status code
 func GetExitStatus() int {
 	return exitStatus
 }
