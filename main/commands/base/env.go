81eeb6569944a288a7db1e889dbbb1fb9d3a57d5
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-02-15 17:00:47 +0800

    chore

--- main/commands/base/env.go
+++ main/commands/base/env.go
@@ -5,15 +5,11 @@ import (
 	"path"
 )
 
-// CommandEnvHolder is a struct holds the environment info of commands
 type CommandEnvHolder struct {
-	// Excutable name of current binary
 	Exec string
-	// commands column width of current command
 	CommandsWidth int
 }
 
-// CommandEnv holds the environment info of commands
 var CommandEnv CommandEnvHolder
 
 func init() {
