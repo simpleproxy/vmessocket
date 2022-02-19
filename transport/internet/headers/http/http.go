324a0f8f5f32d7237abb31c4c4bd4bfcb0b8a86e
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-02-19 19:25:06 +0800

    chore

--- transport/internet/headers/http/http.go
+++ transport/internet/headers/http/http.go
@@ -1,7 +1,5 @@
 package http
 
-//go:generate go run github.com/vmessocket/vmessocket/common/errors/errorgen
-
 import (
 	"bufio"
 	"bytes"
@@ -24,9 +22,36 @@ const (
 
 var (
 	ErrHeaderToLong = newError("Header too long.")
-
 	ErrHeaderMisMatch = newError("Header Mismatch.")
 )
+
+type Authenticator struct {
+	config *Config
+}
+
+type Conn struct {
+	net.Conn
+	readBuffer          *buf.Buffer
+	oneTimeReader       Reader
+	oneTimeWriter       Writer
+	errorWriter         Writer
+	errorMismatchWriter Writer
+	errorTooLongWriter  Writer
+	errReason           error
+}
+
+type HeaderReader struct {
+	req            *http.Request
+	expectedHeader *RequestConfig
+}
+
+type HeaderWriter struct {
+	header *buf.Buffer
+}
+
+type NoOpReader struct{}
+
+type NoOpWriter struct{}
 
 type Reader interface {
 	Read(io.Reader) (*buf.Buffer, error)
@@ -36,21 +61,76 @@ type Writer interface {
 	Write(io.Writer) error
 }
 
-type NoOpReader struct{}
-
-func (NoOpReader) Read(io.Reader) (*buf.Buffer, error) {
-	return nil, nil
+func formResponseHeader(config *ResponseConfig) *HeaderWriter {
+	header := buf.New()
+	common.Must2(header.WriteString(strings.Join([]string{config.GetFullVersion(), config.GetStatusValue().Code, config.GetStatusValue().Reason}, " ")))
+	common.Must2(header.WriteString(CRLF))
+	headers := config.PickHeaders()
+	for _, h := range headers {
+		common.Must2(header.WriteString(h))
+		common.Must2(header.WriteString(CRLF))
+	}
+	if !config.HasHeader("Date") {
+		common.Must2(header.WriteString("Date: "))
+		common.Must2(header.WriteString(time.Now().Format(http.TimeFormat)))
+		common.Must2(header.WriteString(CRLF))
+	}
+	common.Must2(header.WriteString(CRLF))
+	return &HeaderWriter{
+		header: header,
+	}
 }
 
-type NoOpWriter struct{}
-
-func (NoOpWriter) Write(io.Writer) error {
-	return nil
+func NewAuthenticator(ctx context.Context, config *Config) (Authenticator, error) {
+	return Authenticator{
+		config: config,
+	}, nil
 }
 
-type HeaderReader struct {
-	req            *http.Request
-	expectedHeader *RequestConfig
+func NewConn(conn net.Conn, reader Reader, writer Writer, errorWriter Writer, errorMismatchWriter Writer, errorTooLongWriter Writer) *Conn {
+	return &Conn{
+		Conn:                conn,
+		oneTimeReader:       reader,
+		oneTimeWriter:       writer,
+		errorWriter:         errorWriter,
+		errorMismatchWriter: errorMismatchWriter,
+		errorTooLongWriter:  errorTooLongWriter,
+	}
+}
+
+func NewHeaderWriter(header *buf.Buffer) *HeaderWriter {
+	return &HeaderWriter{
+		header: header,
+	}
+}
+
+func (c *Conn) Close() error {
+	if c.oneTimeWriter != nil && c.errorWriter != nil {
+		switch c.errReason {
+		case ErrHeaderMisMatch:
+			c.errorMismatchWriter.Write(c.Conn)
+		case ErrHeaderToLong:
+			c.errorTooLongWriter.Write(c.Conn)
+		default:
+			c.errorWriter.Write(c.Conn)
+		}
+	}
+	return c.Conn.Close()
+}
+
+func (a Authenticator) Client(conn net.Conn) net.Conn {
+	if a.config.Request == nil && a.config.Response == nil {
+		return conn
+	}
+	var reader Reader = NoOpReader{}
+	if a.config.Request != nil {
+		reader = new(HeaderReader)
+	}
+	var writer Writer = NoOpWriter{}
+	if a.config.Response != nil {
+		writer = a.GetClientWriter()
+	}
+	return NewConn(conn, reader, writer, NoOpWriter{}, NoOpWriter{}, NoOpWriter{})
 }
 
 func (h *HeaderReader) ExpectThisRequest(expectedHeader *RequestConfig) *HeaderReader {
@@ -58,13 +138,52 @@ func (h *HeaderReader) ExpectThisRequest(expectedHeader *RequestConfig) *HeaderR
 	return h
 }
 
+func (a Authenticator) GetClientWriter() *HeaderWriter {
+	header := buf.New()
+	config := a.config.Request
+	common.Must2(header.WriteString(strings.Join([]string{config.GetMethodValue(), config.PickURI(), config.GetFullVersion()}, " ")))
+	common.Must2(header.WriteString(CRLF))
+	headers := config.PickHeaders()
+	for _, h := range headers {
+		common.Must2(header.WriteString(h))
+		common.Must2(header.WriteString(CRLF))
+	}
+	common.Must2(header.WriteString(CRLF))
+	return &HeaderWriter{
+		header: header,
+	}
+}
+
+func (a Authenticator) GetServerWriter() *HeaderWriter {
+	return formResponseHeader(a.config.Response)
+}
+
+func (c *Conn) Read(b []byte) (int, error) {
+	if c.oneTimeReader != nil {
+		buffer, err := c.oneTimeReader.Read(c.Conn)
+		if err != nil {
+			c.errReason = err
+			return 0, err
+		}
+		c.readBuffer = buffer
+		c.oneTimeReader = nil
+	}
+	if !c.readBuffer.IsEmpty() {
+		nBytes, _ := c.readBuffer.Read(b)
+		if c.readBuffer.IsEmpty() {
+			c.readBuffer.Release()
+			c.readBuffer = nil
+		}
+		return nBytes, nil
+	}
+	return c.Conn.Read(b)
+}
+
 func (h *HeaderReader) Read(reader io.Reader) (*buf.Buffer, error) {
 	buffer := buf.New()
 	totalBytes := int32(0)
 	endingDetected := false
-
 	var headerBuf bytes.Buffer
-
 	for totalBytes < maxHeaderLength {
 		_, err := buffer.ReadFrom(reader)
 		if err != nil {
@@ -90,12 +209,10 @@ func (h *HeaderReader) Read(reader io.Reader) (*buf.Buffer, error) {
 			}
 		}
 	}
-
 	if !endingDetected {
 		buffer.Release()
 		return nil, ErrHeaderToLong
 	}
-
 	if h.expectedHeader == nil {
 		if buffer.IsEmpty() {
 			buffer.Release()
@@ -103,13 +220,11 @@ func (h *HeaderReader) Read(reader io.Reader) (*buf.Buffer, error) {
 		}
 		return buffer, nil
 	}
-
 	if req, err := readRequest(bufio.NewReader(bytes.NewReader(headerBuf.Bytes())), false); err != nil {
 		return nil, err
 	} else {
 		h.req = req
 	}
-
 	path := h.req.URL.Path
 	hasThisURI := false
 	for _, u := range h.expectedHeader.Uri {
@@ -117,172 +232,18 @@ func (h *HeaderReader) Read(reader io.Reader) (*buf.Buffer, error) {
 			hasThisURI = true
 		}
 	}
-
 	if !hasThisURI {
 		return nil, ErrHeaderMisMatch
 	}
-
 	if buffer.IsEmpty() {
 		buffer.Release()
 		return nil, nil
 	}
-
 	return buffer, nil
 }
 
-type HeaderWriter struct {
-	header *buf.Buffer
-}
-
-func NewHeaderWriter(header *buf.Buffer) *HeaderWriter {
-	return &HeaderWriter{
-		header: header,
-	}
-}
-
-func (w *HeaderWriter) Write(writer io.Writer) error {
-	if w.header == nil {
-		return nil
-	}
-	err := buf.WriteAllBytes(writer, w.header.Bytes())
-	w.header.Release()
-	w.header = nil
-	return err
-}
-
-type Conn struct {
-	net.Conn
-
-	readBuffer          *buf.Buffer
-	oneTimeReader       Reader
-	oneTimeWriter       Writer
-	errorWriter         Writer
-	errorMismatchWriter Writer
-	errorTooLongWriter  Writer
-	errReason           error
-}
-
-func NewConn(conn net.Conn, reader Reader, writer Writer, errorWriter Writer, errorMismatchWriter Writer, errorTooLongWriter Writer) *Conn {
-	return &Conn{
-		Conn:                conn,
-		oneTimeReader:       reader,
-		oneTimeWriter:       writer,
-		errorWriter:         errorWriter,
-		errorMismatchWriter: errorMismatchWriter,
-		errorTooLongWriter:  errorTooLongWriter,
-	}
-}
-
-func (c *Conn) Read(b []byte) (int, error) {
-	if c.oneTimeReader != nil {
-		buffer, err := c.oneTimeReader.Read(c.Conn)
-		if err != nil {
-			c.errReason = err
-			return 0, err
-		}
-		c.readBuffer = buffer
-		c.oneTimeReader = nil
-	}
-
-	if !c.readBuffer.IsEmpty() {
-		nBytes, _ := c.readBuffer.Read(b)
-		if c.readBuffer.IsEmpty() {
-			c.readBuffer.Release()
-			c.readBuffer = nil
-		}
-		return nBytes, nil
-	}
-
-	return c.Conn.Read(b)
-}
-
-func (c *Conn) Write(b []byte) (int, error) {
-	if c.oneTimeWriter != nil {
-		err := c.oneTimeWriter.Write(c.Conn)
-		c.oneTimeWriter = nil
-		if err != nil {
-			return 0, err
-		}
-	}
-
-	return c.Conn.Write(b)
-}
-
-func (c *Conn) Close() error {
-	if c.oneTimeWriter != nil && c.errorWriter != nil {
-		switch c.errReason {
-		case ErrHeaderMisMatch:
-			c.errorMismatchWriter.Write(c.Conn)
-		case ErrHeaderToLong:
-			c.errorTooLongWriter.Write(c.Conn)
-		default:
-			c.errorWriter.Write(c.Conn)
-		}
-	}
-
-	return c.Conn.Close()
-}
-
-func formResponseHeader(config *ResponseConfig) *HeaderWriter {
-	header := buf.New()
-	common.Must2(header.WriteString(strings.Join([]string{config.GetFullVersion(), config.GetStatusValue().Code, config.GetStatusValue().Reason}, " ")))
-	common.Must2(header.WriteString(CRLF))
-
-	headers := config.PickHeaders()
-	for _, h := range headers {
-		common.Must2(header.WriteString(h))
-		common.Must2(header.WriteString(CRLF))
-	}
-	if !config.HasHeader("Date") {
-		common.Must2(header.WriteString("Date: "))
-		common.Must2(header.WriteString(time.Now().Format(http.TimeFormat)))
-		common.Must2(header.WriteString(CRLF))
-	}
-	common.Must2(header.WriteString(CRLF))
-	return &HeaderWriter{
-		header: header,
-	}
-}
-
-type Authenticator struct {
-	config *Config
-}
-
-func (a Authenticator) GetClientWriter() *HeaderWriter {
-	header := buf.New()
-	config := a.config.Request
-	common.Must2(header.WriteString(strings.Join([]string{config.GetMethodValue(), config.PickURI(), config.GetFullVersion()}, " ")))
-	common.Must2(header.WriteString(CRLF))
-
-	headers := config.PickHeaders()
-	for _, h := range headers {
-		common.Must2(header.WriteString(h))
-		common.Must2(header.WriteString(CRLF))
-	}
-	common.Must2(header.WriteString(CRLF))
-	return &HeaderWriter{
-		header: header,
-	}
-}
-
-func (a Authenticator) GetServerWriter() *HeaderWriter {
-	return formResponseHeader(a.config.Response)
-}
-
-func (a Authenticator) Client(conn net.Conn) net.Conn {
-	if a.config.Request == nil && a.config.Response == nil {
-		return conn
-	}
-	var reader Reader = NoOpReader{}
-	if a.config.Request != nil {
-		reader = new(HeaderReader)
-	}
-
-	var writer Writer = NoOpWriter{}
-	if a.config.Response != nil {
-		writer = a.GetClientWriter()
-	}
-	return NewConn(conn, reader, writer, NoOpWriter{}, NoOpWriter{}, NoOpWriter{})
+func (NoOpReader) Read(io.Reader) (*buf.Buffer, error) {
+	return nil, nil
 }
 
 func (a Authenticator) Server(conn net.Conn) net.Conn {
@@ -295,10 +256,29 @@ func (a Authenticator) Server(conn net.Conn) net.Conn {
 		formResponseHeader(resp400))
 }
 
-func NewAuthenticator(ctx context.Context, config *Config) (Authenticator, error) {
-	return Authenticator{
-		config: config,
-	}, nil
+func (c *Conn) Write(b []byte) (int, error) {
+	if c.oneTimeWriter != nil {
+		err := c.oneTimeWriter.Write(c.Conn)
+		c.oneTimeWriter = nil
+		if err != nil {
+			return 0, err
+		}
+	}
+	return c.Conn.Write(b)
+}
+
+func (w *HeaderWriter) Write(writer io.Writer) error {
+	if w.header == nil {
+		return nil
+	}
+	err := buf.WriteAllBytes(writer, w.header.Bytes())
+	w.header.Release()
+	w.header = nil
+	return err
+}
+
+func (NoOpWriter) Write(io.Writer) error {
+	return nil
 }
 
 func init() {
