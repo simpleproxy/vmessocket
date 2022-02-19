f023ccc32f4152266ef9cc3c7ecd17c06b246def
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-02-19 19:56:32 +0800

    ch

--- transport/pipe/reader.go
+++ transport/pipe/reader.go
@@ -10,14 +10,14 @@ type Reader struct {
 	pipe *pipe
 }
 
+func (r *Reader) Interrupt() {
+	r.pipe.Interrupt()
+}
+
 func (r *Reader) ReadMultiBuffer() (buf.MultiBuffer, error) {
 	return r.pipe.ReadMultiBuffer()
 }
 
 func (r *Reader) ReadMultiBufferTimeout(d time.Duration) (buf.MultiBuffer, error) {
 	return r.pipe.ReadMultiBufferTimeout(d)
-}
-
-func (r *Reader) Interrupt() {
-	r.pipe.Interrupt()
 }
