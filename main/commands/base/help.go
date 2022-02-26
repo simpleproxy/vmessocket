92bd1ac86844ccfc65f675bcf58d41127f72ec96
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-02-26 23:05:22 +0800

    chore

--- main/commands/base/help.go
+++ main/commands/base/help.go
@@ -1,7 +1,3 @@
-// Copyright 2017 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package base
 
 import (
@@ -16,7 +12,6 @@ import (
 	"unicode/utf8"
 )
 
-// Help implements the 'help' command.
 func Help(w io.Writer, args []string) {
 	cmd := RootCommand
 Args:
@@ -27,17 +22,14 @@ Args:
 				continue Args
 			}
 		}
-
-		// helpSuccess is the help command using as many args as possible that would succeed.
 		helpSuccess := CommandEnv.Exec + " help"
 		if i > 0 {
 			helpSuccess += " " + strings.Join(args[:i], " ")
 		}
 		fmt.Fprintf(os.Stderr, "%s help %s: unknown help topic. Run '%s'.\n", CommandEnv.Exec, strings.Join(args, " "), helpSuccess)
-		SetExitStatus(2) // failed at 'v2ray help cmd'
+		SetExitStatus(2)
 		Exit()
 	}
-
 	if len(cmd.Commands) > 0 {
 		PrintUsage(os.Stdout, cmd)
 	} else {
@@ -71,7 +63,6 @@ var helpTemplate = `{{if .Runnable}}usage: {{.UsageLine}}
 {{end}}{{.Long | trim}}
 `
 
-// An errWriter wraps a writer, recording whether a write error occurred.
 type errWriter struct {
 	w   io.Writer
 	err error
@@ -85,7 +76,6 @@ func (w *errWriter) Write(b []byte) (int, error) {
 	return n, err
 }
 
-// tmpl executes the given template text on data, writing the result to w.
 func tmpl(w io.Writer, text string, data interface{}) {
 	t := template.New("top")
 	t.Funcs(template.FuncMap{"trim": strings.TrimSpace, "capitalize": capitalize, "width": width})
@@ -93,7 +83,6 @@ func tmpl(w io.Writer, text string, data interface{}) {
 	ew := &errWriter{w: w}
 	err := t.Execute(ew, data)
 	if ew.err != nil {
-		// I/O error writing. Ignore write on closed pipe.
 		if strings.Contains(ew.err.Error(), "pipe") {
 			SetExitStatus(1)
 			Exit()
@@ -118,7 +107,6 @@ func width(width int, value string) string {
 	return fmt.Sprintf(format, value)
 }
 
-// PrintUsage prints usage of cmd to w
 func PrintUsage(w io.Writer, cmd *Command) {
 	buildCommandText(cmd)
 	bw := bufio.NewWriter(w)
@@ -126,14 +114,9 @@ func PrintUsage(w io.Writer, cmd *Command) {
 	bw.Flush()
 }
 
-// buildCommandText build command text as template
 func buildCommandText(cmd *Command) {
 	data := makeTmplData(cmd)
 	cmd.UsageLine = buildText(cmd.UsageLine, data)
-	// DO NOT SUPPORT ".Short":
-	// - It's not necessary
-	// - Or, we have to build text for all sub commands of current command, like "v2ray help api"
-	// cmd.Short = buildText(cmd.Short, data)
 	cmd.Long = buildText(cmd.Long, data)
 }
 
@@ -150,7 +133,6 @@ type tmplData struct {
 }
 
 func makeTmplData(cmd *Command) tmplData {
-	// Minimum width of the command column
 	width := 12
 	for _, c := range cmd.Commands {
 		l := len(c.Name())
