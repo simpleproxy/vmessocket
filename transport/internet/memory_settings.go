59854af7a8e5d0fbb4a9466d0a1ba0386ec66660
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-27 16:51:27 +0800

    Update memory_settings.go

--- transport/internet/memory_settings.go
+++ transport/internet/memory_settings.go
@@ -19,10 +19,6 @@ func ToMemoryStreamConfig(s *StreamConfig) (*MemoryStreamConfig, error) {
 		ProtocolSettings: ets,
 	}
 
-	if s != nil {
-		mss.SocketSettings = s.SocketSettings
-	}
-
 	if s != nil && s.HasSecuritySettings() {
 		ess, err := s.GetEffectiveSecuritySettings()
 		if err != nil {
