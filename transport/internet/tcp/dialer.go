22a73f650b12d85b49c19b0e235c2d9e32721fd2
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-03-28 15:19:09 +0800

    tcp

--- transport/internet/tcp/dialer.go
+++ transport/internet/tcp/dialer.go
@@ -7,7 +7,6 @@ import (
 	"github.com/vmessocket/vmessocket/common/net"
 	"github.com/vmessocket/vmessocket/common/session"
 	"github.com/vmessocket/vmessocket/transport/internet"
-	"github.com/vmessocket/vmessocket/transport/internet/tls"
 )
 
 func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.MemoryStreamConfig) (internet.Connection, error) {
@@ -15,10 +14,6 @@ func Dial(ctx context.Context, dest net.Destination, streamSettings *internet.Me
 	conn, err := internet.DialSystem(ctx, dest, streamSettings.SocketSettings)
 	if err != nil {
 		return nil, err
-	}
-	if config := tls.ConfigFromStreamSettings(streamSettings); config != nil {
-		tlsConfig := config.GetTLSConfig(tls.WithDestination(dest))
-		conn = tls.Client(conn, tlsConfig)
 	}
 	return internet.Connection(conn), nil
 }
